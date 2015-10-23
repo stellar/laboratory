531adeed5ec0ee352dfbb515052d6e4bfad3084a
irisli <github@iris.li>
2015-10-23 15:47:56 -0700

    fix is-active missing space

--- src/components/EndpointListItem.js
+++ src/components/EndpointListItem.js
@@ -3,7 +3,7 @@ import {ExplorerActions} from '../actions/ExplorerActions';
 
 export let EndpointListItem = React.createClass({
   render: function() {
-    return <li className={'s-button s-button__light' + (this.props.endpoint.selected ? 'is-active' : '')}
+    return <li className={'s-button s-button__light' + (this.props.endpoint.selected ? ' is-active' : '')}
       onClick={this.props.onSelect.bind(null, this.props.endpoint.id)}>
         {this.props.endpoint.text}
       </li>;
