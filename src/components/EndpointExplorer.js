4f96eeccc88699c478123eda5e8c7b8fcad86c84
irisli <github@iris.li>
2015-11-03 16:12:01 -0800

    add more of TransactionBuilder front end

--- src/components/EndpointExplorer.js
+++ src/components/EndpointExplorer.js
@@ -5,18 +5,22 @@ import {EndpointResult} from './EndpointResult';
 
 export let EndpointExplorer = React.createClass({
   render: function() {
-    return <div className="EndpointExplorer">
-      <div className="EndpointExplorer__picker">
-        <EndpointPicker />
-      </div>
+    return <div className="so-back">
+      <div className="so-chunk">
+        <div className="EndpointExplorer">
+          <div className="EndpointExplorer__picker">
+            <EndpointPicker />
+          </div>
 
-      <div className="EndpointExplorer__setup">
-        <EndpointSetup />
-      </div>
+          <div className="EndpointExplorer__setup">
+            <EndpointSetup />
+          </div>
 
-      <div className="EndpointExplorer__result">
-        <EndpointResult />
+          <div className="EndpointExplorer__result">
+            <EndpointResult />
+          </div>
+        </div>
       </div>
-    </div>;
+    </div>
   }
 });
