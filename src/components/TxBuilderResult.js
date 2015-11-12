d83d573ae0536b58674a7ac09e433392ceb96524
irisli <github@iris.li>
2015-11-11 18:08:23 -0800

    hook up xdr result to ui

--- src/components/TxBuilderResult.js
+++ src/components/TxBuilderResult.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {AddressParameter} from './ParametersFormComponents/AddressParameter';
+import TxBuilderStore from '../stores/TxBuilderStore';
 
 export default class TxBuilderResult extends React.Component {
+  onUpdate() {
+    this.forceUpdate();
+  }
+  componentDidMount() {
+    TxBuilderStore.addUpdateListener(this.onUpdate);
+  }
+  componentWillUnmount() {
+    TxBuilderStore.removeUpdateListener(this.onUpdate);
+  }
   render() {
+    let xdr = TxBuilderStore.getXdr();
     return <div className="TxBuilderResult">
       <h3>Resulting TransactionEnvelope XDR:</h3>
-      <pre className="TransactionXDR so-code TransactionBuilderResult__code"><code>AAAAAGXNhB2hIkbP//jgzn4os/AAAAZAB+BaLPAAA5Q/xL</code></pre>
+      <pre className="TransactionXDR so-code TransactionBuilderResult__code"><code>{xdr}</code></pre>
       <button className="s-button">Sign this transaction</button>
     </div>
   }
