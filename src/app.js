2e3ca100a31cb0e1ba3dd3d588ce530e0146d2c8
irisli <github@iris.li>
2015-11-17 10:27:32 -0800

    redux WIP

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@ require("./styles/main.scss");
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import rootReducer from './reducers/root';
 import {PlaygroundChrome} from './components/PlaygroundChrome';
 
 document.write('<div id="app"></div>');
 
+let store = createStore(rootReducer);
+
 ReactDOM.render(
-  <PlaygroundChrome />,
+  <Provider store={store}>
+    <PlaygroundChrome />
+  </Provider>,
   document.getElementById('app')
 );
