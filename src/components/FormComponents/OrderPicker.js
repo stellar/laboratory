8a9517684ead166209c093996b22c1ce4f1164f6
Iris Li <github@iris.li>
2015-12-03 14:11:19 -0800

    comment out default buttin in OrderPicker

--- src/components/FormComponents/OrderPicker.js
+++ src/components/FormComponents/OrderPicker.js
@@ -5,11 +5,14 @@ export default PickerGenerator({
   fields: [{
     type: 'radio',
     options: [
-      {
-        name: 'default', // Just used internally for keys. Must be unique within this set of options
-        label: 'default (asc)', // Displayed to the user (can't be used for keys in case of localzation)
-        value: 'default', // Must be non-empty string
-      },
+      // TODO: support the default button. Currently not possible because we
+      // can't have an empty string a the value or else the validator will think
+      // it is not filled out.
+      // {
+      //   name: 'default', // Just used internally for keys. Must be unique within this set of options
+      //   label: 'default (asc)', // Displayed to the user (can't be used for keys in case of localzation)
+      //   value: 'default', // Must be non-empty string
+      // },
       {
         name: 'asc',
         label: 'asc',
@@ -22,7 +25,7 @@ export default PickerGenerator({
       },
     ],
     name: 'order',
-    default: 'default', // optional. matches with options[].value
+    // default: 'default', // optional. matches with options[].value
     validator: value => null,
   }],
 });
