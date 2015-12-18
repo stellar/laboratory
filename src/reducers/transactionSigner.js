cbeea2225a36ce32f9299cf364f9ffd69458b517
Iris Li <github@iris.li>
2015-12-18 14:54:41 -0800

    remove default testing transaction in transaction signer

--- src/reducers/transactionSigner.js
+++ src/reducers/transactionSigner.js
@@ -14,8 +14,8 @@ const transactionSigner = combineReducers({
 export default transactionSigner;
 
 function tx(state = {
-  xdr: 'AAAAAAJ6JsqETD62vmfnwnunIbeHkngYuSSR622WfZmHpz9yAAAAyAAAAAAAAATTAAAAAAAAAAEAAAAGdGV3ZmRzAAAAAAACAAAAAQAAAAD5NMv6q1ZjBafbOC4BmHn5UNVYlacnl8kooX5xIOPpYgAAAAEAAAAArqW21g0d1YLqh1F/m+mzhXj5w6T0v0bHkxIwvVllnZEAAAACMjM1MjM0dwAAAAAAAAAAAAJ6JsqETD62vmfnwnunIbeHkngYuSSR622WfZmHpz9yAAAAAA9/IfAAAAAAAAAAAAAAAAACeibKhEw+tr5n58J7pyG3h5J4GLkkkettln2Zh6c/cgAAAAAAmJaAAAAAAAAAAAA=',
-  loaded: true,
+  xdr: '',
+  loaded: false,
 }, action) {
   switch (action.type) {
   case IMPORT_FROM_XDR:
