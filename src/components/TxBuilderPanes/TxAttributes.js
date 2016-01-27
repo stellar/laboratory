dc87d6465c1f030c8517fd21938ce5a5eb972c95
Iris Li <github@iris.li>
2016-01-27 14:07:40 -0800

    improve base fee clarity

--- src/components/TxBuilderPanes/TxAttributes.js
+++ src/components/TxBuilderPanes/TxAttributes.js
@@ -21,11 +21,12 @@ export default function TxAttributes(props) {
         />
       <p className="optionsTable__pair__content__note">The transaction sequence number is usually one higher than current account sequence number.</p>
     </OptionsTablePair>
-    <OptionsTablePair label="Transaction Fee" optional={true}>
+    <OptionsTablePair label="Base Fee" optional={true}>
       <StroopsPicker
         value={props.values['fee']}
         onUpdate={(value) => {props.onUpdate('fee', value)}}
         />
+      <p className="optionsTable__pair__content__note">The <a href="https://www.stellar.org/developers/learn/concepts/fees.html">network base fee</a> is currently set to 100 stroops (0.00001 XLM). Transaction fee is equal to base fee times number of operations in this transaction.</p>
     </OptionsTablePair>
     <OptionsTablePair label="Memo" optional={true}>
       <MemoPicker
