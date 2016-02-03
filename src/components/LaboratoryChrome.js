226965b055250d24cf587b98a4b5d07b3d008a99
Iris Li <github@iris.li>
2016-02-03 16:26:48 -0500

    hook up routing to LaboratoryChrome

--- src/components/LaboratoryChrome.js
+++ src/components/LaboratoryChrome.js
@@ -7,74 +7,72 @@ import TransactionBuilder from './TransactionBuilder';
 import TransactionSigner from './TransactionSigner';
 import {RouterListener} from '../utilities/simpleRouter';
 
-let LaboratoryChrome = React.createClass({
-  getInitialState: function() {
-    return {
-      tab: 'EndpointExplorer'
-    };
-  },
-  setTab: function(tab) {
-    this.setState({tab})
-  },
-  render: function() {
-    let activeTab;
-    switch (this.state.tab) {
-      case 'EndpointExplorer':
-        activeTab = <EndpointExplorer />;
-        break;
-      case 'TransactionBuilder':
-        activeTab = <TransactionBuilder />;
-        break;
-      case 'TransactionSigner':
-        activeTab = <TransactionSigner />;
-        break;
-      default:
-        throw new Error(`Invalid tab: ${this.state.tab}`);
-    }
-
-    let defaultClasses = 'buttonList__item s-button s-button__min';
-
-    let tabItem = (name, key) => {
-      return <a
-        onClick={() => {this.setTab(key)}}
-        className={classNames(
-          'buttonList__item s-button s-button__min',
-          {'is-active': this.state.tab === key})}
-        key={key}>
-        {name}
-      </a>
-    }
-
-    return <div>
-      <div className="so-back">
-        <div className="so-chunk">
-          <div className="so-siteHeader LaboratoryChrome__header">
-            <span className="so-logo">
-              <a href="https://www.stellar.org/" className="so-logo__main">Stellar</a>
-              <span className="so-logo__separator"> </span>
-              <a href="https://www.stellar.org/laboratory/" className="so-logo__subSite">laboratory</a>
-            </span>
-            <NetworkPicker />
-          </div>
-        </div>
-      </div>
-      <div className="so-back LaboratoryChrome__siteNavBack">
-        <div className="so-chunk">
-          <nav className="s-buttonList">
-            {tabItem('Endpoint Explorer', 'EndpointExplorer')}
-            {tabItem('Transaction Builder', 'TransactionBuilder')}
-            {tabItem('Transaction Signer', 'TransactionSigner')}
-          </nav>
-        </div>
-      </div>
-
-      {activeTab}
-      <RouterListener />
-    </div>;
+function LaboratoryChrome(props) {
+  let tabItem = (name, slug) => {
+    return <a
+      href={'#' + slug}
+      className={classNames(
+        'buttonList__item s-button s-button__min',
+        {'is-active': props.routing.location === slug})}
+      key={slug}>
+      {name}
+    </a>
   }
-});
+
+  return <div>
+    <div className="so-back">
+      <div className="so-chunk">
+        <div className="so-siteHeader LaboratoryChrome__header">
+          <span className="so-logo">
+            <a href="https://www.stellar.org/" className="so-logo__main">Stellar</a>
+            <span className="so-logo__separator"> </span>
+            <a href="https://www.stellar.org/laboratory/" className="so-logo__subSite">laboratory</a>
+          </span>
+          <NetworkPicker />
+        </div>
+      </div>
+    </div>
+    <div className="so-back LaboratoryChrome__siteNavBack">
+      <div className="so-chunk">
+        <nav className="s-buttonList">
+          {tabItem('Endpoint Explorer', 'endpoints')}
+          {tabItem('Transaction Builder', 'txbuilder')}
+          {tabItem('Transaction Signer', 'txsigner')}
+        </nav>
+      </div>
+    </div>
+
+    {getContent(props.routing.location)}
+    <RouterListener />
+  </div>;
+}
+
+function getContent(slug) {
+  switch (slug) {
+    case '':
+      return <SimplePage><p>To begin, select a tool from above.</p></SimplePage>
+    case 'endpoints':
+      return <EndpointExplorer />;
+    case 'txbuilder':
+      return <TransactionBuilder />;
+    case 'txsigner':
+      return <TransactionSigner />;
+    default:
+      return <SimplePage><p>Page "{slug}" not found</p></SimplePage>
+  }
+}
+
+function SimplePage(props) {
+  return <div className="so-back SimplePage__back">
+    <div className="so-chunk">
+      {props.children}
+    </div>
+  </div>
+}
 
 export default connect(chooseState)(LaboratoryChrome);
 function chooseState(state) {
-  return {}
+  return {
+    routing: state.routing
+  }
 }
