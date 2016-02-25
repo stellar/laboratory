5de701ecc863e608db2ac06cb65a528bd74c6e75
Iris Li <github@iris.li>
2016-02-25 15:10:16 -0800

    use dynamically generated list of xdr types; common ones at top and also duplicated near bottom of the list

--- src/components/FormComponents/SelectPicker.js
+++ src/components/FormComponents/SelectPicker.js
@@ -1,20 +1,33 @@
 import React from 'react';
+import _ from 'lodash';
 
-// @param {object} props.items An object whose keys are the onUpdate values and
+// @param {object|array} props.items An object whose keys are the onUpdate values and
 //   object values are labels the user will see.
 // {
 //   'valueToBeStored': 'Human Readable Label',
 // }
+//
+// For array values, the UI label and onUpdate values are the same.
+// The benefits array bring is that it can contain duplicate items.
 export default function SelectPicker(props) {
   let {value, onUpdate, items} = props;
+
+  let optionsList;
+  if (_.isArray(items)) {
+    optionsList = _.map(items, (value, index) => {
+      return <option key={index} value={value}>{value}</option>
+    })
+  } else {
+    optionsList = _.map(items, (value, index) => {
+      return <option key={index} value={index}>{value}</option>
+    })
+  }
   return <select
     className="picker picker--select"
     value={value}
     onChange={(event) => onUpdate(event.target.value)}
     >
     <option value=""></option>
-    {_.map(items, (label, id) => {
-      return <option key={id} value={id}>{label}</option>
-    })}
+    {optionsList}
   </select>;
 }
