df375cd87fe0bf279b6fad07fdae88b5f3acfc34
Iris Li <github@iris.li>
2016-03-04 14:49:08 -0800

    implement syntax highlighting in CodeBlock

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import Prism from '../utilities/prism.js';
+
+// @param {string} language - key for the code language. available languages can
+//   be discovered by doing a console log on `Prism.languages`. More can be added
+//   in the prism.js
 
 export let CodeBlock = React.createClass({
   propTypes: {
     code: React.PropTypes.string.isRequired,
     language: React.PropTypes.string,
   },
-  getHighlightedCode: function() {
-    return this.props.code;
+  shouldComponentUpdate: function(nextProps, nextState) {
+    return nextProps.code !== this.props.code || nextProps.language !== this.props.language;
   },
-  // // Uncomment for performance improvements after verifying it works
-  // shouldComponentUpdate: function(nextProps, nextState) {
-  //   return nextProps.code !== this.props.code || nextProps.language !== this.props.language;
-  // }
+  getHighlightedCode: function() {
+    return Prism.highlight(this.props.code, Prism.languages[this.props.language]);
+  },
   render: function() {
-    return <pre className="CodeBlock"><code className="CodeBlock__code">{this.getHighlightedCode()}</code></pre>;
+    let className = 'CodeBlock__code language-' + this.props.language;
+    let innerHtml = { __html: this.getHighlightedCode() };
+    return <pre className="CodeBlock">
+      <code className={className} dangerouslySetInnerHTML={innerHtml}></code>
+    </pre>;
   }
 })
