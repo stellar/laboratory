086eb71fe3c8ba58b822493ee71c9cee8d88740b
Iris Li <github@iris.li>
2016-03-07 15:14:41 -0800

    add scroll reset when user clicks on link in the JSON response

--- src/components/CodeBlock.js
+++ src/components/CodeBlock.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Prism from '../utilities/prism.js';
 import jsonLinkHighlighter from '../utilities/prism-jsonLinkHighlighter';
+import scrollOnAnchorOpen from '../utilities/scrollOnAnchorOpen';
 
 // @param {string} language - key for the code language. available languages can
 //   be discovered by doing a console log on `Prism.languages`. More can be added
@@ -24,7 +25,7 @@ export let CodeBlock = React.createClass({
   render: function() {
     let className = 'CodeBlock__code language-' + this.props.language;
     let innerHtml = { __html: this.getHighlightedCode() };
-    return <pre className="CodeBlock">
+    return <pre className="CodeBlock" onClick={scrollOnAnchorOpen}>
       <code className={className} dangerouslySetInnerHTML={innerHtml}></code>
     </pre>;
   }
