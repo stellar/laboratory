b6b7e2296aeda7f83d9dca2fbd328b6c6ee6bfc6
Iris Li <github@iris.li>
2016-03-10 12:09:51 -0800

    improve new context dispatcher

--- src/actions/endpointExplorer.js
+++ src/actions/endpointExplorer.js
@@ -31,13 +31,7 @@ export const START_REQUEST = "START_REQUEST"
 export const ERROR_REQUEST = "ERROR_REQUEST"
 export const UPDATE_REQUEST = "UPDATE_REQUEST"
 let resultIdNonce = 0;
-let openStream, openIntervalId;
-function clearOpenInterval() {
-  if (typeof openIntervalId !== 'undefined') {
-    window.clearInterval(openIntervalId);
-    openIntervalId = undefined;
-  }
-}
+let openStream;
 export function submitRequest(request) {
   return dispatch => {
     // Close old stream if it exists
@@ -45,26 +39,12 @@ export function submitRequest(request) {
       openStream.close();
       openStream = null;
     }
-    clearOpenInterval();
 
     let id = resultIdNonce++;
     dispatch({
       type: START_REQUEST,
       id,
     });
-
-    // Calling `dispatch` inside the `.catch` of `httpRequest` causes problems
-    // because it silently hides errors down the stack including all the React
-    // re-rendering happening due to the dispatch.
-    // dispatchStack is implemented so that the dispatch function is invoked in
-    // a separate stack thereby avoiding catching irrelevant errors
-    let dispatchObj;
-    openIntervalId = setInterval(() => {
-      if (typeof dispatchObj !== 'undefined') {
-        clearOpenInterval();
-        dispatch(dispatchObj);
-      }
-    }, 100);
 
     if (request.streaming) {
       openStream = streamingRequest(request.url, (message) => {
@@ -79,14 +59,14 @@ export function submitRequest(request) {
       // dispatchObj will only be called at most one time.
       httpRequest(request)
         .then(r => {
-          dispatchObj = ({
+          dispatchInNewContext(dispatch, {
             type: UPDATE_REQUEST,
             id,
             body: r.data,
           });
         })
         .catch(e => {
-          dispatch({
+          dispatchInNewContext(dispatch, {
             type: ERROR_REQUEST,
             id,
             errorStatus: e.status,
@@ -95,6 +75,16 @@ export function submitRequest(request) {
         });
     }
   }
+}
+
+// Calling `dispatch` inside the `.catch` of `httpRequest` causes problems
+// because it silently hides errors down the stack including all the React
+// re-rendering happening due to the dispatch.
+// By dispatching in a new context, the dispatch function won't be caught
+function dispatchInNewContext(dispatch, dispatchObj) {
+  setTimeout(() => {
+    dispatch(dispatchObj);
+  }, 0);
 }
 
 function httpRequest(request) {
