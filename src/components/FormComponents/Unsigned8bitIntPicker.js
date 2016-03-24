6f1f371975c8a9e73080dd33c2f311434e089282
Iris Li <github@iris.li>
2016-03-24 15:01:33 -0700

    improve placeholder for unsigned 8 bit integer picker

--- src/components/FormComponents/Unsigned8bitIntPicker.js
+++ src/components/FormComponents/Unsigned8bitIntPicker.js
@@ -4,9 +4,10 @@ import PositiveIntPicker from './PositiveIntPicker';
 export default function Unsigned8bitIntPicker(props) {
   return <PositiveIntPicker
     {...props}
+    placeholder="0 - 255"
     validator={(value) => {
       if (value >= 255) {
-        return 'Expected a number between 0 and 255 (inclusive).';
+        return 'Expected an integer between 0 and 255 (inclusive).';
       }
     }}
   />
