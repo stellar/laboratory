8b7cc9c67b22b4589e6cc60863535a602509ced3
Iris Li <github@iris.li>
2016-03-28 13:56:44 -0700

    improve path payment operation setup

--- src/components/OperationPanes/PathPayment.js
+++ src/components/OperationPanes/PathPayment.js
@@ -8,22 +8,33 @@ import ManualMultiPicker from '../FormComponents/ManualMultiPicker.js';
 
 export default function PathPayment(props) {
   return [
+    <OptionsTablePair label="Destination" key="destination">
+      <PubKeyPicker
+        value={props.values['destination']}
+        onUpdate={(value) => {props.onUpdate('destination', value)}}
+        />
+    </OptionsTablePair>,
     <OptionsTablePair label="Sending Asset" key="sendAsset">
       <AssetPicker
         value={props.values['sendAsset']}
         onUpdate={(value) => {props.onUpdate('sendAsset', value)}}
         />
+      <p className="optionsTable__pair__content__note">The asset to be deduced from the sender's account</p>
     </OptionsTablePair>,
     <OptionsTablePair label="Maximum send amount" key="sendMax">
       <AmountPicker
         value={props.values['sendMax']}
         onUpdate={(value) => {props.onUpdate('sendMax', value)}}
         />
+      <p className="optionsTable__pair__content__note">The most the sender is willing to spend to take the paths to. Resulting amount may vary due to the offers in the orderbook.</p>
     </OptionsTablePair>,
-    <OptionsTablePair label="Destination" key="destination">
-      <PubKeyPicker
-        value={props.values['destination']}
-        onUpdate={(value) => {props.onUpdate('destination', value)}}
+    <OptionsTablePair label="Intermediate Path" key="path" optional="true">
+      <ManualMultiPicker
+        component={AssetPicker}
+        value={props.values['path']}
+        default={{}}
+        addNewLabel="Add new intermediate asset"
+        onUpdate={(value) => {props.onUpdate('path', value)}}
         />
     </OptionsTablePair>,
     <OptionsTablePair label="Destination Asset" key="destAsset">
@@ -31,6 +42,7 @@ export default function PathPayment(props) {
         value={props.values['destAsset']}
         onUpdate={(value) => {props.onUpdate('destAsset', value)}}
         />
+      <p className="optionsTable__pair__content__note">The asset to be received by the destination account</p>
     </OptionsTablePair>,
     <OptionsTablePair label="Destination Amount" key="destAmount">
       <AmountPicker
@@ -38,14 +50,5 @@ export default function PathPayment(props) {
         onUpdate={(value) => {props.onUpdate('destAmount', value)}}
         />
     </OptionsTablePair>,
-    <OptionsTablePair label="Asset Path" key="path">
-      <ManualMultiPicker
-        component={AssetPicker}
-        value={props.values['path']}
-        default={{}}
-        onUpdate={(value) => {props.onUpdate('path', value)}}
-        />
-    </OptionsTablePair>,
-
   ];
 }
