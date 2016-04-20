716fb67fea3d6f7ccfb8c3e1dc3d2ffb55cf4533
Iris Li <github@iris.li>
2016-04-20 13:38:48 -0700

    add transaction signer and xdr test cases; make other tests more clear

--- test/unit/routingRoundTrip.js
+++ test/unit/routingRoundTrip.js
@@ -3,6 +3,7 @@ import {stateToQueryObj, queryObjToLoadStatePayload} from '../../src/utilities/s
 import SLUG from '../../src/constants/slug';
 import rootReducer from '../../src/reducers/root';
 import {loadState} from '../../src/actions/routing';
+import {dehydrate} from '../../src/utilities/hydration';
 
 // The routing system converts the state into a url, which gets converted back
 // into the state thus forming an infinite loop. In general, the starting state
@@ -10,32 +11,42 @@ import {loadState} from '../../src/actions/routing';
 // deserialization, and loading process is working correctly.
 // @param {string} slug - slug constant
 // @param {object} queryObj - the url query object that we start from and end at
-function itCircularlyTransformsState(slug, queryObj) {
+// @param {object} (endingqueryObj) - In some situations, we expect the round
+//   trip to end with more parameters it started with because the routing system
+//   ensures that some parameters are always set in the url. For example, we
+//   always have the network setting in the url.
+function itCircularlyConvertsState(slug, queryObj, endingQueryObj) {
+  if (endingQueryObj === undefined) {
+    endingQueryObj = queryObj;
+  }
+
   it('circularly converts [query obj] to [state] to initial [query obj]', () => {
     let resultQueryObj = stateToQueryObj(slug,
       rootReducer(undefined,
         loadState(slug,
-          queryObjToLoadStatePayload(slug, queryObj)
+          queryObj
         )
       )
     );
-    expect(resultQueryObj).to.deep.equal(queryObj);
+    expect(resultQueryObj).to.deep.equal(endingQueryObj);
   });
 }
 
 describe('routing system', () => {
   describe('for endpoint explorer', () => {
     describe('in an empty scenario', () => {
-      itCircularlyTransformsState(SLUG.EXPLORER, {
+      itCircularlyConvertsState(SLUG.EXPLORER, {
         network: 'test',
       });
     });
 
     describe('in a configured scenario', () => {
-      itCircularlyTransformsState(SLUG.EXPLORER, {
+      itCircularlyConvertsState(SLUG.EXPLORER, {
         endpoint: 'all',
         resource: 'transactions',
-        values: 'eyJjdXJzb3IiOiI1In0=',
+        values: dehydrate({
+          cursor: 5,
+        }),
         network: 'test',
       });
     });
@@ -43,15 +54,72 @@ describe('routing system', () => {
 
   describe('for transaction builder', () => {
     describe('in a default scenario', () => {
-      itCircularlyTransformsState(SLUG.TXBUILDER, {
+      itCircularlyConvertsState(SLUG.TXBUILDER, {
         network: 'test',
       });
     });
 
     describe('in a configured scenario', () => {
-      itCircularlyTransformsState(SLUG.TXBUILDER, {
+      itCircularlyConvertsState(SLUG.TXBUILDER, {
         network: 'test',
-        params: 'eyJhdHRyaWJ1dGVzIjp7InNvdXJjZUFjY291bnQiOiJzb21ld2hlcmUiLCJzZXF1ZW5jZSI6Im92ZXIiLCJtZW1vVHlwZSI6Ik1FTU9fVEVYVCIsIm1lbW9Db250ZW50IjoidGhlIn0sIm9wZXJhdGlvbnMiOlt7ImlkIjowLCJhdHRyaWJ1dGVzIjp7ImRlc3RpbmF0aW9uIjoicmFpbmJvdyJ9LCJuYW1lIjoicGF5bWVudCJ9XX0=',
+        params: dehydrate({
+          attributes: {
+            sourceAccount: 'somewhere',
+            sequence: 'over',
+            memoType: 'MEMO_TEXT',
+            memoContent: 'the'
+          },
+          operations: [{
+            id: 0,
+            attributes: {
+              destination: 'rainbow'
+            },
+            name: 'payment'
+          }]
+        }),
+      });
+    });
+  });
+
+  describe('for transaction signer', () => {
+    describe('in an empty scenario', () => {
+      itCircularlyConvertsState(SLUG.TXSIGNER, {
+        network: 'test',
+      });
+    });
+
+    describe('in a configured scenario', () => {
+      itCircularlyConvertsState(SLUG.TXSIGNER, {
+        network: 'test',
+        xdr: 'AAAAAOviDWZdAxfxZ83i1Dx6ZGbnSM8pxoPMrtc5VuSJSBKsAAAAZAAAAAAAAAABAAAAAAAAAAEAAAAAAAAAAQAAAAAAAAAJAAAAAAAAAAA=',
+      });
+    });
+  });
+
+  describe('for XDR viewer', () => {
+    describe('in an empty scenario', () => {
+      itCircularlyConvertsState(SLUG.XDRVIEWER, {
+        network: 'test',
+      }, {
+        network: 'test',
+        type: 'TransactionEnvelope',
+      });
+    });
+
+    describe('in a configured scenario', () => {
+      itCircularlyConvertsState(SLUG.XDRVIEWER, {
+        network: 'test',
+        type: 'TransactionEnvelope',
+        input: 'AAAAAOviDWZdAxfxZ83i1Dx6ZGbnSM8pxoPMrtc5VuSJSBKsAAAAZAAAAAAAAAABAAAAAAAAAAEAAAAAAAAAAQAAAAAAAAAJAAAAAAAAAAA=',
+      });
+    });
+  });
+
+  describe('for any page', () => {
+    describe('where network is not specified and should be added ', () => {
+      itCircularlyConvertsState(SLUG.EXPLORER, {
+      }, {
+        network: 'test',
       });
     });
   });
