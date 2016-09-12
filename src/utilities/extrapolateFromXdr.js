3bb46519a7ecc3267154f596d48f04d6393898ef
Bartek Nowotarski <bartek@nowotarski.info>
2016-09-13 01:00:06 +0200

    SignatureHint explanation and fix

--- src/utilities/extrapolateFromXdr.js
+++ src/utilities/extrapolateFromXdr.js
@@ -99,14 +99,45 @@ function getValue(object, name) {
   }
 
   if (name === 'hint') {
+    // strkey encoding is using base32 encoding. Encoded public key consists of:
+    //
+    //  * 1 byte version byte (0x30 encoded as `G`)
+    //  * 32 bytes public key
+    //  * 2 bytes checksum
+    //
+    // Because base32 symbols are 5-bit, more than one symbol is needed to represent a single byte.
+    // Signature Hint is the last 4 bytes of the public key. So we need to try to show as many 5-bit
+    // chunks as possible included between bytes 30 and 33 (included).
+    //
+    // byte 1: ##### ###
+    // byte 2:          ## ##### #
+    // byte 3:                    #### ####
+    // byte 4:                             # ##### ##
+    // byte 5:                                       ### #####  <---------- 40 bits / full alignment
+    // byte 6:                                                ##### ###
+    // byte 7:                                                         ## ##### #
+    //
+    // .....
+    //
+    // byte 26: ##### ###
+    // byte 27:          ## ##### #
+    // byte 28:                    #### ####                    full b32 symbols
+    // byte 29:                             # ##### ##    |--------------------------|
+    // byte 30:                                       ### 48###                      |
+    // byte 31:                  Signature Hint start |        49### 50#             |    Signature Hint end
+    // byte 32:                                                         ## 51### 5   |    |
+    // byte 33:                                                                   2### 53##
+    // byte 34:                                                                            # 54### 55
+    // byte 35:                                                                                      ### 56###
+    //
     let hintBytes = new Buffer(object, 'base64');
     let partialPublicKey = Buffer.concat([new Buffer(28).fill(0), hintBytes]);
     let keypair = new Keypair({publicKey: partialPublicKey});
     let partialPublicKeyString =
       'G'+
       (new Buffer(46).fill('_').toString())+
-      keypair.accountId().substr(47, 4)+
-      (new Buffer(5).fill('_').toString());
+      keypair.accountId().substr(47, 5)+
+      (new Buffer(4).fill('_').toString());
     return {type: 'code', value: partialPublicKeyString};
   }
 
