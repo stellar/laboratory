19e1a9c27128d6eec46ac7248a01a60c824e7952
Jed McCaleb <swamp12@yahoo.com>
2016-11-18 11:59:09 -0800

    Update GenericOffer.js

--- src/components/OperationPanes/GenericOffer.js
+++ src/components/OperationPanes/GenericOffer.js
@@ -20,14 +20,14 @@ export default function GenericOffer(props) {
         onUpdate={(value) => {props.onUpdate('buying', value)}}
         />
     </OptionsTablePair>,
-    <OptionsTablePair label="Amount" key="amount">
+    <OptionsTablePair label="Amount you are selling" key="amount">
       <AmountPicker
         value={props.values['amount']}
         onUpdate={(value) => {props.onUpdate('amount', value)}}
         />
       <p className="optionsTable__pair__content__note">An amount of zero will delete the offer.</p>
     </OptionsTablePair>,
-    <OptionsTablePair label="Price" key="price">
+    <OptionsTablePair label="Price of 1 unit of asset for sale" key="price">
       <PositiveNumberPicker
         value={props.values['price']}
         onUpdate={(value) => {props.onUpdate('price', value)}}
