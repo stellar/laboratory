8d4c7d593a5b7d07a404d7a66adb56c08ea0032a
robertdurst <rsdurst@colby.edu>
2018-01-31 16:20:50 -0800

    Persistant network selection across tabs

--- src/reducers/network.js
+++ src/reducers/network.js
@@ -10,11 +10,16 @@ export default network;
 function current(state=NETWORK.defaultName, action) {
   switch(action.type) {
     case LOAD_STATE:
+      const lsCurrentNetwork = localStorage.getItem('CURRENT_NETWORK');
       if (action.queryObj.network && NETWORK.available[action.queryObj.network]) {
         return action.queryObj.network;
       }
+      if (lsCurrentNetwork) {
+        return lsCurrentNetwork;
+      }
       return state;
     case CHOOSE_NETWORK:
+      localStorage.setItem('CURRENT_NETWORK', action.name);
       return action.name;
     default:
       return state;
