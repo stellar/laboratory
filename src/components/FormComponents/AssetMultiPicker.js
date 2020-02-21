243c514d422edfb2cdbd9451629a2bce879617c0
Carl Vitullo <carl@stellar.org>
2020-02-21 16:04:08 -0500

    Simplify key

--- src/components/FormComponents/AssetMultiPicker.js
+++ src/components/FormComponents/AssetMultiPicker.js
@@ -12,7 +12,12 @@ export default function AssetMultiPicker({
     <>
       {value.map((asset, index) => (
         <div
-          key={`${asset === "native" ? "native" : "issued"}${index}`}
+          // index here is an okay key because AssetPicker has no local state.
+          // There isn't a natural value to use as a key here, because basing it
+          // off the data passed in would lead to it changing on  user input
+          // (which would also break local state if AssetPicker had any). If this
+          // becomes a problem we'll have to revisit the strategy here.
+          key={index}
           className="MultiPicker__container picker--spaceBottom"
         >
           <button
