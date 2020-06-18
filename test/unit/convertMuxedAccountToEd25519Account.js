f3c25caeead22f29ef1c4ecbfbc91bbb5d8979df
Adolfo Builes <builes.adolfo@gmail.com>
2020-06-18 14:09:24 -0500

    Update convertMuxedAccountToEd25519Account to return the same value it receives.

--- test/unit/convertMuxedAccountToEd25519Account.js
+++ test/unit/convertMuxedAccountToEd25519Account.js
@@ -1,12 +1,12 @@
 import convert from  '../../src/utilities/convertMuxedAccountToEd25519Account';
 
 describe('convertMuxedAccountToEd25519Account', () => {
-    it('converts a M account to a G key', () => {
+    it('returns M accounts for now', () => {
       expect(convert('MAAAAAAAAAAAAFHU4MODGMHQOYHMPCMPOZ3KIL3ZH34AUH4MH6UG2AJEBFWQOPSVR2RMK'))
-        .to.equal('GD2OGHBTGDYHMDWHRGHXM5VEF54T56AKD6GD7KDNAESAS3IHHZKY46XI');
+        .to.equal('MAAAAAAAAAAAAFHU4MODGMHQOYHMPCMPOZ3KIL3ZH34AUH4MH6UG2AJEBFWQOPSVR2RMK');
     });
     it('works with G accounts', () => {
-        expect(convert('MAAAAAAAAAAAAFHU4MODGMHQOYHMPCMPOZ3KIL3ZH34AUH4MH6UG2AJEBFWQOPSVR2RMK'))
+        expect(convert('GD2OGHBTGDYHMDWHRGHXM5VEF54T56AKD6GD7KDNAESAS3IHHZKY46XI'))
           .to.equal('GD2OGHBTGDYHMDWHRGHXM5VEF54T56AKD6GD7KDNAESAS3IHHZKY46XI');
     });
 });
