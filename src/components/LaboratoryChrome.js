c0bacdabc5bca986e7931825098889f99d285512
Carl Vitullo <carl@stellar.org>
2020-07-20 17:16:15 -0400

    Make async chunk failure error prettier (#468)

--- src/components/LaboratoryChrome.js
+++ src/components/LaboratoryChrome.js
@@ -1,3 +1,6 @@
+/**
+ * @prettier
+ */
 import React from "react";
 import { connect } from "react-redux";
 import classNames from "classnames";
@@ -73,13 +76,29 @@ function LaboratoryChrome(props) {
   );
 }
 
-const Loading = ({ pastDelay, retry, error }) => {
+const Loading = ({ pastDelay, error }) => {
   if (error) {
     return (
       <div className="so-back">
-        <div className="so-chunk">
-          Error! <button onClick={retry}>Retry</button>
-          <pre>{error.stack}</pre>
+        <div className="so-chunk Introduction">
+          <p>
+            There was a problem loading this page. If your network connection is
+            still working, try reloading.
+          </p>
+          <p>
+            <button
+              className="s-button"
+              onClick={() => {
+                window.history.go(0);
+              }}
+            >
+              Reload
+            </button>
+          </p>
+          <details>
+            <summary>View stack trace</summary>
+            <pre>{error.stack}</pre>
+          </details>
         </div>
       </div>
     );
