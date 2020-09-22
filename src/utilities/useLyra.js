82d64a3a223c25f1a98ea4c5305c2eb855ed69f4
Jeesun Kim <jeesunkim@Jeesuns-MacBook-Pro.local>
2020-09-22 16:15:10 -0400

    lyra-feature-address-input-btn > calling Lyra's getPublicKey() api to get a public key; updated its svg button to include alt like text

--- src/utilities/useLyra.js
+++ src/utilities/useLyra.js
@@ -1,6 +1,6 @@
 /** @format */
 import React from "react";
-import { isConnected } from "@stellar/lyra-api";
+import { isConnected, getPublicKey } from "@stellar/lyra-api";
 
 export const useLyra = () => {
   const [hasLyra, setHasLyra] = React.useState(false);
@@ -9,4 +9,17 @@ export const useLyra = () => {
     setHasLyra(isConnected());
   });
   return hasLyra;
+};
+
+export const lyraGetPublicKey = async (onUpdate) => {
+  try {
+    const lyraResponse = await getPublicKey();
+
+    if (lyraResponse.error) {
+      return lyraResponse.error;
+    }
+    return onUpdate(lyraResponse.publicKey);
+  } catch (e) {
+    console.log("error while getting a public key: ", e);
+  }
 };
