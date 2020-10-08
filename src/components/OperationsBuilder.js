4123ced00c1c36dcf4d22716b2f9e2de8abf3a4e
Iveta <quietbits@gmail.com>
2020-10-08 11:38:05 -0400

    Fix opConfig.excludeAccountSource

--- src/components/OperationsBuilder.js
+++ src/components/OperationsBuilder.js
@@ -49,7 +49,7 @@ let operation = (ops, index, dispatch) => {
       <a href={opConfig.docsUrl} target="_blank">See documentation for {opConfig.label}</a>
     </p>;
 
-    sourceAccountRow = <OptionsTablePair label="Source Account" optional={true} key="sourceAccount">
+    sourceAccountRow = opConfig.excludeAccountSource ? null : <OptionsTablePair label="Source Account" optional={true} key="sourceAccount">
       <PubKeyPicker
         value={op.attributes['sourceAccount']}
         onUpdate={(value) => dispatchUpdateOpAtts('sourceAccount', value)}
@@ -87,7 +87,7 @@ let operation = (ops, index, dispatch) => {
       </OptionsTablePair>
       {separator}
       {operationPane}
-      {!opConfig.excludeAccountSource && sourceAccountRow}
+      {sourceAccountRow}
     </div>
   </div>;
 }
