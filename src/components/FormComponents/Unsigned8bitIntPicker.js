c35bfcc09a5cdd1969ab381cac53b94de020402b
Iveta <quietbits@users.noreply.github.com>
2021-11-04 14:10:58 -0400

    Fix: allow max value 255 in Set Options operation (#617)

--- src/components/FormComponents/Unsigned8bitIntPicker.js
+++ src/components/FormComponents/Unsigned8bitIntPicker.js
@@ -7,7 +7,7 @@ export default function Unsigned8bitIntPicker(props) {
       {...props}
       placeholder="0 - 255"
       validator={(value) => {
-        if (value >= 255) {
+        if (value > 255) {
           return "Expected an integer between 0 and 255 (inclusive).";
         }
       }}
