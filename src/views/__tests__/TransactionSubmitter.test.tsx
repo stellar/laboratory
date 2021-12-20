86ff8d64a8c311e877c7ab147eb90c563e61e585
Piyal Basu <pbasu235@gmail.com>
2021-12-20 18:54:17 -0500

    adding tests

--- src/views/__tests__/TransactionSubmitter.test.tsx
+++ src/views/__tests__/TransactionSubmitter.test.tsx
@@ -5,10 +5,23 @@
 import { fireEvent, waitFor, screen } from "@testing-library/react";
 import { render } from "helpers/testHelpers";
 import TransactionSubmitter from "views/TransactionSubmitter";
+import { defaultNetwork } from "reducers/network";
+// import * as xdrViewerActions from "actions/xdrViewer";
+// import FETCHED_SIGNERS from "constants/fetched_signers";
 // import { MOCK_SIGNED_TRANSACTION } from "./__mocks__/transactions";
 
+// jest.spyOn(xdrViewerActions, "fetchSigners").mockImplementation(
+//   () => (dispatch) =>
+//     dispatch({
+//       type: FETCHED_SIGNERS.SUCCESS,
+//       result: signature,
+//     }),
+// );
+
 test("renders TransactionSubmitter page", async () => {
-  render(<TransactionSubmitter />);
+  render(<TransactionSubmitter />, {
+    preloadedState: { network: { current: defaultNetwork } },
+  });
   await waitFor(() => screen.getByTestId("page-transaction-submitter"));
   expect(screen.getByTestId("page-transaction-submitter")).toBeInTheDocument();
 });
@@ -26,12 +39,12 @@ test("shows error on invalid xdr", () => {
   );
 });
 
-// test("shows error on invalid xdr", () => {
-//   render(<TransactionSubmitter />);
-//   fireEvent.change(screen.getByTestId("transaction-submitter-input"), {
-//     target: { value: "foo" },
-//   });
-//   expect(screen.getByTestId("transaction-submitter-alert")).toHaveTextContent(
-//     "Unable to decode input as TransactionEnvelope",
-//   );
-// });
+test("shows error on invalid xdr", () => {
+  render(<TransactionSubmitter />);
+  fireEvent.change(screen.getByTestId("transaction-submitter-input"), {
+    target: { value: "foo" },
+  });
+  expect(screen.getByTestId("transaction-submitter-alert")).toHaveTextContent(
+    "Unable to decode input as TransactionEnvelope",
+  );
+});
