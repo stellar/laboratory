f3088d5791a1d7a221e71bf9a75a30a4e596933e
Piyal Basu <pbasu235@gmail.com>
2022-01-14 19:50:03 -0500

    add test for xdr extrapolation

--- src/views/__tests__/XdrViewer.test.tsx
+++ src/views/__tests__/XdrViewer.test.tsx
@@ -5,6 +5,8 @@
 import { fireEvent, waitFor, screen } from "@testing-library/react";
 import { render } from "helpers/testHelpers";
 import XdrViewer from "views/XdrViewer";
+import * as extrapolateFromXdr from "helpers/extrapolateFromXdr";
+import { MOCK_SIGNED_TRANSACTION } from "./__mocks__/transactions";
 
 test("renders XDR Viewer page", async () => {
   render(<XdrViewer />);
@@ -15,12 +17,26 @@ test("renders XDR Viewer page", async () => {
   );
 });
 
-test("shows error on bad input", () => {
+test("shows error on bad input", async () => {
   render(<XdrViewer />);
+  await waitFor(() => screen.getByTestId("page-xdr-viewer"));
   fireEvent.change(screen.getByTestId("xdr-viewer-input"), {
     target: { value: "foo" },
   });
   expect(screen.getByTestId("xdr-viewer-results")).toHaveTextContent(
     "Unable to decode input as TransactionEnvelope",
+  );
+});
+
+test("extrapolate XDR on input change", async () => {
+  const extrapolateFromXdrSpy = jest.spyOn(extrapolateFromXdr, "default");
+  render(<XdrViewer />);
+  await waitFor(() => screen.getByTestId("page-xdr-viewer"));
+  fireEvent.change(screen.getByTestId("xdr-viewer-input"), {
+    target: { value: MOCK_SIGNED_TRANSACTION },
+  });
+  expect(extrapolateFromXdrSpy).toHaveBeenCalledWith(
+    MOCK_SIGNED_TRANSACTION,
+    "TransactionEnvelope",
   );
 });
