556f17ecdf9d455288be6dc1381084aca792e722
Piyal Basu <pbasu235@gmail.com>
2022-10-10 17:13:44 -0400

    Revert "add soroban sdk tx signer view (#664)"
    This reverts commit 5af3901a6aadb44cc6ed86e76cb69b630e7219a8.

--- src/components/SorobanBanner.tsx
+++ src/components/SorobanBanner.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { useRedux } from "hooks/useRedux";
 import { useIsSoroban } from "hooks/useIsSoroban";
-import SLUG from "constants/slug";
 
 export const SorobanBanner = () => {
   const { routing } = useRedux("routing");
 
   let isSoroban = useIsSoroban();
 
-  return isSoroban &&
-    (routing.location === SLUG.XDRVIEWER ||
-      routing.location === SLUG.TXSIGNER) ? (
+  return isSoroban && routing.location === "xdr-viewer" ? (
     <div className="LaboratoryChrome__soroban_alert s-alert">
       <div className="so-chunk">
         You are on Soroban network and can view Soroban XDR
--- src/hooks/useIsSoroban.ts
+++ src/hooks/useIsSoroban.ts
@@ -6,11 +6,10 @@ const futureNetPassphrase = "Test SDF Future Network ; October 2022";
 export const useIsSoroban = () => {
   const { network } = useRedux("network", "routing");
   const { horizonURL, networkPassphrase } = network.current;
-  const url = new URL(horizonURL);
 
   let isOnFuturenet = false;
   if (
-    url.origin === futureNetUrl &&
+    horizonURL === futureNetUrl &&
     networkPassphrase === futureNetPassphrase
   ) {
     isOnFuturenet = true;
