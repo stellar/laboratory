01377251a2074e3db1a66506a6de37c91c0a492c
Iveta <quietbits@gmail.com>
2024-02-28 09:01:11 -0500

    V2 Maintenance banner

---
+++ src/query/QueryProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
+
+export const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
--- src/types/types.ts
+++ src/types/types.ts
@@ -9,3 +9,25 @@ export type Network = {
   rpcUrl: string;
   passphrase: string;
 };
+
+export type StatusPageComponent = {
+  [key: string]: any;
+  id: string;
+  name: string;
+};
+
+export type StatusPageIncident = {
+  [key: string]: any;
+  id: string;
+  name: string;
+  body: string;
+};
+
+export type StatusPageScheduled = {
+  [key: string]: any;
+  id: string;
+  name: string;
+  scheduled_for: string;
+  components: StatusPageComponent[];
+  incident_updates: StatusPageIncident[];
+};
