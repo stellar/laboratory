0dbc504a522c2f73f2eb5fe4caf6d6347dda020b
Jeesun Kim <jeesunkim@Jeesuns-MBP.localdomain>
2024-03-04 14:46:12 -0800

    [wip] create account

--- src/app/(sidebar)/account/create/page.tsx
+++ src/app/(sidebar)/account/create/page.tsx
@@ -1,5 +1,29 @@
 "use client";
 
+import { Card, Link, Text, Button, Icon } from "@stellar/design-system";
+
+import { NextLink } from "@/components/NextLink";
+
 export default function CreateAccount() {
-  return <div>Create Account</div>;
+  return (
+    <Card>
+      <div className="CardText">
+        <Text size="lg" as="h1" weight="medium">
+          Keypair Generator
+        </Text>
+
+        <Text size="sm" as="p">
+          These keypairs can be used on the Stellar network where one is
+          required. For example, it can be used as an account master key,
+          account signer, and/or as a stellar-core node key.
+        </Text>
+      </div>
+
+      <div className="padding-top-30">
+        <Button size="md" variant="tertiary">
+          Generate keypair
+        </Button>
+      </div>
+    </Card>
+  );
 }
