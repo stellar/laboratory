7b1d31638724173c7b62e252a6cc3c452285c373
Jeesun Kim <jeesunkim@Jeesuns-MBP.localdomain>
2024-03-14 16:29:46 -0700

    fix based on eslint

--- src/app/(sidebar)/explore-endpoints/layout.tsx
+++ src/app/(sidebar)/explore-endpoints/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import { LayoutSidebarContent } from "@/components/layout/LayoutSidebarContent";
 import { Routes } from "@/constants/routes";
 
--- src/app/(sidebar)/layout.tsx
+++ src/app/(sidebar)/layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { LayoutWithSidebar } from "@/components/layout/LayoutWithSidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
