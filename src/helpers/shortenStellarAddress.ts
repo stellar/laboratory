8680b256486b5295aa469277e5028225c8a40e78
Jeesun Kim <jeesunkim@Jeesuns-MacBook-Pro.local>
2024-03-18 21:37:59 -0700

    const instead of function

--- src/helpers/shortenStellarAddress.ts
+++ src/helpers/shortenStellarAddress.ts
@@ -1,7 +1,7 @@
-export function shortenStellarAddress(address: string, size: number = 8) {
+export const shortenStellarAddress = (address: string, size: number = 8) => {
   const slice = Math.ceil(size / 2);
 
   return `${address.substring(0, slice)}…${address.substring(
     address.length - slice,
   )}`;
-}
+};
