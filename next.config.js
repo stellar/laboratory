c5345d85635664d7a7b015e55cc1b0d373ac7db6
Iveta <quietbits@gmail.com>
2024-03-20 16:43:00 -0400

    Undo middleware

--- next.config.js
+++ next.config.js
@@ -6,4 +6,39 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// Need to allow unsafe-eval in development for code refresh.
+const cspHeader = `
+    default-src 'self';
+    connect-src 'self' https://9sl3dhr1twv1.statuspage.io/api/v2/;
+    script-src 'self' 'strict-dynamic' ${
+      process.env.NODE_ENV === "production"
+        ? ""
+        : `'unsafe-eval' 'unsafe-inline'`
+    };
+    style-src 'self' https://fonts.googleapis.com ${process.env.NODE_ENV === "production" ? "" : `'unsafe-inline'`};
+    img-src 'self' blob: data:;
+    font-src 'self' https://fonts.gstatic.com/;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+};
