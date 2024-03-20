4d069d9cdcf29f1dc4b9a5822228bec14e2a0252
Iveta <quietbits@gmail.com>
2024-03-20 15:03:42 -0400

    Add Content Security Policy config

--- next.config.js
+++ next.config.js
@@ -6,4 +6,37 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+// Need to allow unsafe-eval in development for code refresh.
+const cspHeader = `
+    default-src 'self';
+    connect-src 'self' https://9sl3dhr1twv1.statuspage.io/api/v2/;
+    script-src 'self' 'unsafe-inline' ${
+      process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+    };
+    style-src 'self' https://fonts.googleapis.com 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self' https://fonts.gstatic.com/;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
+  },
+};
