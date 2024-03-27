f5f5e32de153230010927b8e843d2f5099fd869e
Jeesun Kim <jeesunkim@Jeesuns-MacBook-Pro.local>
2024-03-27 14:22:58 -0700

    unused

--- src/app/(sidebar)/account/muxed-parse/page.tsx
+++ src/app/(sidebar)/account/muxed-parse/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Alert, Card, Input, Text, Button } from "@stellar/design-system";
+import { Alert, Card, Text, Button } from "@stellar/design-system";
 
 import { useStore } from "@/store/useStore";
 
