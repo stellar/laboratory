894ef23ed2288f38e2c224ab80c46dad0d2c888c
Iveta <quietbits@gmail.com>
2024-04-09 14:35:14 -0400

    Build transaction: basic tabs setup

--- src/app/(sidebar)/transaction/build/page.tsx
+++ src/app/(sidebar)/transaction/build/page.tsx
@@ -1,5 +1,55 @@
 "use client";
 
+import { Alert, Card } from "@stellar/design-system";
+import { TabView } from "@/components/TabView";
+import { useStore } from "@/store/useStore";
+import { Box } from "@/components/layout/Box";
+
 export default function BuildTransaction() {
-  return <div>Build Transaction</div>;
+  const { transaction } = useStore();
+  const { activeTab } = transaction.build;
+  const { updateBuildActiveTab } = transaction;
+
+  const renderAttributes = () => {
+    return (
+      <Box gap="md">
+        <>
+          <Card>Attributes</Card>
+
+          <Alert variant="primary" placement="inline">
+            The transaction builder lets you build a new Stellar transaction.
+            This transaction will start out with no signatures. To make it into
+            the ledger, this transaction will then need to be signed and
+            submitted to the network.
+          </Alert>
+        </>
+      </Box>
+    );
+  };
+
+  const renderOperations = () => {
+    return <Card>Operations</Card>;
+  };
+
+  return (
+    <div>
+      <TabView
+        heading={{ title: "Build Transaction" }}
+        tab1={{
+          id: "attributes",
+          label: "Attributes",
+          content: renderAttributes(),
+        }}
+        tab2={{
+          id: "operations",
+          label: "Operations",
+          content: renderOperations(),
+        }}
+        activeTabId={activeTab}
+        onTabChange={(id) => {
+          updateBuildActiveTab(id);
+        }}
+      />
+    </div>
+  );
 }
