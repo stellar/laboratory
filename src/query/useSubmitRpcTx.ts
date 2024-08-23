5d1afa485151f0f46db722b945b23dde8112c60a
Iveta <quietbits@users.noreply.github.com>
2024-08-23 15:08:01 -0400

    Submit transaction: handle exception (#982)
    * Submit transaction: handle exception
    
    * Response UI tweaks
    
    * Update connect-src links

--- src/query/useSubmitRpcTx.ts
+++ src/query/useSubmitRpcTx.ts
@@ -20,52 +20,64 @@ export const useSubmitRpcTx = () => {
       transactionXdr,
       networkPassphrase,
     }: SubmitRpcTxProps) => {
-      const transaction = TransactionBuilder.fromXDR(
-        transactionXdr,
-        networkPassphrase,
-      );
-      const sentTx = await rpcServer.sendTransaction(transaction);
+      try {
+        const transaction = TransactionBuilder.fromXDR(
+          transactionXdr,
+          networkPassphrase,
+        );
+        const sentTx = await rpcServer.sendTransaction(transaction);
 
-      if (sentTx.status !== "PENDING") {
-        throw { status: sentTx.status, result: sentTx };
-      }
-
-      let txResponse;
-      const MAX_ATTEMPTS = 10;
-      let attempts = 0;
-
-      while (attempts++ < MAX_ATTEMPTS && txResponse?.status !== "SUCCESS") {
-        txResponse = await rpcServer.getTransaction(sentTx.hash);
-
-        switch (txResponse.status) {
-          case "FAILED":
-            throw { status: "FAILED", result: txResponse };
-          case "NOT_FOUND":
-            await delay(1000);
-            continue;
-          case "SUCCESS":
-          default:
-          // Do nothing
+        if (sentTx.status !== "PENDING") {
+          throw { status: sentTx.status, result: sentTx };
         }
+
+        let txResponse;
+        const MAX_ATTEMPTS = 10;
+        let attempts = 0;
+
+        while (attempts++ < MAX_ATTEMPTS && txResponse?.status !== "SUCCESS") {
+          txResponse = await rpcServer.getTransaction(sentTx.hash);
+
+          switch (txResponse.status) {
+            case "FAILED":
+              throw { status: "FAILED", result: txResponse };
+            case "NOT_FOUND":
+              await delay(1000);
+              continue;
+            case "SUCCESS":
+            default:
+            // Do nothing
+          }
+        }
+
+        if (attempts >= MAX_ATTEMPTS || txResponse?.status !== "SUCCESS") {
+          throw { status: "TIMEOUT", result: txResponse };
+        }
+
+        const submittedTx = TransactionBuilder.fromXDR(
+          txResponse.envelopeXdr,
+          networkPassphrase,
+        );
+        // TS doesn't recognize operations property even though it is there
+        const operations = (submittedTx as any)?.operations || ([] as any[]);
+
+        return {
+          hash: sentTx.hash,
+          result: txResponse,
+          operationCount: operations.length,
+          fee: submittedTx.fee,
+        };
+      } catch (e) {
+        throw {
+          status: "ERROR",
+          result: {
+            status: "ERROR",
+            latestLedger: "",
+            latestLedgerCloseTime: "",
+            errorResult: e,
+          },
+        };
       }
-
-      if (attempts >= MAX_ATTEMPTS || txResponse?.status !== "SUCCESS") {
-        throw { status: "TIMEOUT", result: txResponse };
-      }
-
-      const submittedTx = TransactionBuilder.fromXDR(
-        txResponse.envelopeXdr,
-        networkPassphrase,
-      );
-      // TS doesn't recognize operations property even though it is there
-      const operations = (submittedTx as any)?.operations || ([] as any[]);
-
-      return {
-        hash: sentTx.hash,
-        result: txResponse,
-        operationCount: operations.length,
-        fee: submittedTx.fee,
-      };
     },
   });
 
