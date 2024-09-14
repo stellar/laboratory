9a1a945b56a8fb057d5b4678b1fc12b2cc7b9aa2
Jeesun Kim <jeesunkim@Jeesuns-MacBook-Pro.local>
2024-09-13 17:38:28 -0700

    rename useGetLatestLedger

--- src/query/useLatestLedger.ts
+++ src/query/useLatestLedger.ts
@@ -4,13 +4,12 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useLatestLedger = ({ rpcUrl }: { rpcUrl: string }) => {
   const query = useQuery({
-    queryKey: ["useGetLatestLedger"],
+    queryKey: ["useLatestLedger"],
     queryFn: async () => {
       const rpcServer = new SorobanRpc.Server(rpcUrl);
 
       try {
         const latestLedger = await rpcServer.getLatestLedger();
-
         return latestLedger.sequence;
       } catch (error) {
         throw `there was an error with fetching latest ledger. e: ${error}`;
