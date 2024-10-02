7dd799429c04bd0c15903a177801dcf054901499
Jeesun Kim <3912060+jeesunikim@users.noreply.github.com>
2024-10-02 16:43:42 -0700

    Update 'Soroban RPC' to 'Stellar RPC'

--- tests/endpointsPage.test.ts
+++ tests/endpointsPage.test.ts
@@ -10,8 +10,8 @@ test.describe("API Explorer page", () => {
   });
 
   test("Renders info cards", async ({ page }) => {
-    await expect(page.getByTestId("info-cards").locator("h2")).toHaveText([
-      "Soroban RPC Methods",
+    await expect(page.locator("h2")).toHaveText([
+      "Stellar RPC Methods",
       "Horizon Endpoints",
     ]);
   });
