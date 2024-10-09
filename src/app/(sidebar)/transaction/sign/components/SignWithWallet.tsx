e599c0f426c85e6038ec06858ca584842ddb059e
jeesun 지선 <jeesunikim@users.noreply.github.com>
2024-10-09 10:09:18 -0700

    Add the wallet kit to the top bar (#1080)

--- src/app/(sidebar)/transaction/sign/components/SignWithWallet.tsx
+++ src/app/(sidebar)/transaction/sign/components/SignWithWallet.tsx
@@ -1,33 +1,15 @@
 "use client";
 
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useContext } from "react";
 import { Button } from "@stellar/design-system";
-import {
-  StellarWalletsKit,
-  WalletNetwork,
-  allowAllModules,
-  ISupportedWallet,
-  FREIGHTER_ID,
-} from "@creit.tech/stellar-wallets-kit";
+import { ISupportedWallet } from "@creit.tech/stellar-wallets-kit";
+
+import { shortenStellarAddress } from "@/helpers/shortenStellarAddress";
+import { getWalletKitNetwork } from "@/helpers/getWalletKitNetwork";
 
 import { useStore } from "@/store/useStore";
 
-import { NetworkType } from "@/types/types";
-
-const getWalletNetwork = (network: NetworkType) => {
-  switch (network) {
-    case "testnet":
-      return WalletNetwork.TESTNET;
-    case "mainnet":
-      return WalletNetwork.PUBLIC;
-    case "futurenet":
-      return WalletNetwork.FUTURENET;
-    // @TODO: stellar wallets kit doesn't support CUSTOM
-    //   case "custom":
-    default:
-      return WalletNetwork.TESTNET;
-  }
-};
+import { WalletKitContext } from "@/components/WalletKitContextProvider";
 
 export const SignWithWallet = ({
   setSignError,
@@ -36,14 +18,12 @@ export const SignWithWallet = ({
   setSignError: Dispatch<SetStateAction<string>>;
   setSignSuccess: Dispatch<SetStateAction<string>>;
 }) => {
-  const { network, transaction } = useStore();
+  const { account, network, transaction } = useStore();
   const { sign, updateSignedTx } = transaction;
+  const { walletKitPubKey, updateWalletKitPubKey } = account;
+  const networkPassphrase = getWalletKitNetwork(network.id);
 
-  const kit: StellarWalletsKit = new StellarWalletsKit({
-    network: getWalletNetwork(network.id),
-    selectedWalletId: FREIGHTER_ID,
-    modules: allowAllModules(),
-  });
+  const walletKitInstance = useContext(WalletKitContext);
 
   const onSignWithWallet = async () => {
     // remove the previously signed tx from the display
@@ -51,37 +31,62 @@ export const SignWithWallet = ({
     setSignError("");
     setSignSuccess("");
 
-    await kit.openModal({
-      onWalletSelected: async (option: ISupportedWallet) => {
-        try {
-          kit.setWallet(option.id);
-          const { address } = await kit.getAddress();
-          const networkType = getWalletNetwork(network.id);
-
-          const { signedTxXdr } = await kit.signTransaction(sign.importXdr, {
-            // You could send multiple public keys in case the wallet needs to handle multi signatures
-            address,
-            networkPassphrase: networkType,
+    if (walletKitPubKey) {
+      try {
+        const { signedTxXdr } =
+          await walletKitInstance.walletKit!.signTransaction(sign.importXdr, {
+            address: walletKitPubKey,
+            networkPassphrase,
           });
-
-          updateSignedTx(signedTxXdr);
-          setSignSuccess("1 signature(s) added");
-        } catch (error: any) {
-          // the error for the following wallets:
-          // xbull
-          // albedo
-          // freighter
-          if (error?.message) {
-            setSignError(error?.message);
-          }
+        updateSignedTx(signedTxXdr);
+        setSignSuccess("1 signature(s) added");
+      } catch (error: any) {
+        if (error?.message) {
+          setSignError(error?.message);
         }
-      },
-    });
+      }
+    } else {
+      // if a user didn't log in via stellar wallet kit in the main nav
+      // open a wallet kit modal to sign in
+      await walletKitInstance.walletKit!.openModal({
+        onWalletSelected: async (option: ISupportedWallet) => {
+          try {
+            walletKitInstance.walletKit?.setWallet(option.id);
+            const addressResult =
+              await walletKitInstance.walletKit?.getAddress();
+
+            if (addressResult?.address) {
+              updateWalletKitPubKey(addressResult.address);
+
+              const result = await walletKitInstance.walletKit?.signTransaction(
+                sign.importXdr,
+                {
+                  // You could send multiple public keys in case the wallet needs to handle multi signatures
+                  address: addressResult.address,
+                  networkPassphrase,
+                },
+              );
+
+              if (result?.signedTxXdr) {
+                updateSignedTx(result.signedTxXdr);
+                setSignSuccess("1 signature added");
+              } else {
+                throw { message: "couldn't sign with wallet. try again later" };
+              }
+            }
+          } catch (error: any) {
+            if (error?.message) {
+              setSignError(error?.message);
+            }
+          }
+        },
+      });
+    }
   };
 
-  return (
+  return walletKitPubKey ? (
     <Button size="md" variant="tertiary" onClick={onSignWithWallet}>
-      Sign with wallet
+      Sign with {shortenStellarAddress(walletKitPubKey)}
     </Button>
-  );
+  ) : null;
 };
--- src/store/createStore.ts
+++ src/store/createStore.ts
@@ -17,6 +17,7 @@ import {
   MuxedAccount,
   TxnOperation,
   OpBuildingError,
+  ThemeColorType,
 } from "@/types/types";
 
 export type FeeBumpParams = {
@@ -57,6 +58,8 @@ export type SignTxActiveView = "import" | "overview";
 export interface Store {
   // Shared
   network: Network | EmptyObj;
+  // Theme Color
+  theme: ThemeColorType | undefined;
   // isDynamicNetworkSelect flag to indicate network update outside of the dropdown
   isDynamicNetworkSelect: boolean;
   selectNetwork: (network: Network) => void;
@@ -65,13 +68,14 @@ export interface Store {
   //   set to false when changing network from the dropdown, no need to clear it
   //     in other places because it will stay for the session (not saved in URL)
   updateIsDynamicNetworkSelect: (isDynamic: boolean) => void;
+  setTheme: (theme: ThemeColorType) => void;
   resetStoredData: () => void;
 
   // Account
   account: {
     publicKey: string | undefined;
     secretKey: string | undefined;
-
+    walletKitPubKey: string | undefined;
     generatedMuxedAccountInput: Partial<MuxedAccount> | EmptyObj;
     parsedMuxedAccountInput: string | undefined;
     generatedMuxedAccount: MuxedAccount | EmptyObj;
@@ -81,6 +85,7 @@ export interface Store {
     updateParsedMuxedAccountInput: (value: string) => void;
     updateGeneratedMuxedAccount: (value: MuxedAccount) => void;
     updateParsedMuxedAccount: (value: MuxedAccount) => void;
+    updateWalletKitPubKey: (value?: string) => void;
     reset: () => void;
   };
 
@@ -241,6 +246,7 @@ const initTransactionState = {
 const initAccountState = {
   publicKey: undefined,
   secretKey: undefined,
+  walletKitPubKey: undefined,
   generatedMuxedAccountInput: {},
   parsedMuxedAccountInput: undefined,
   generatedMuxedAccount: {},
@@ -261,6 +267,7 @@ export const createStore = (options: CreateStoreOptions) =>
       immer((set) => ({
         // Shared
         network: {},
+        theme: undefined,
         isDynamicNetworkSelect: false,
         selectNetwork: (network: Network) =>
           set((state) => {
@@ -269,6 +276,10 @@ export const createStore = (options: CreateStoreOptions) =>
         updateIsDynamicNetworkSelect: (isDynamic: boolean) =>
           set((state) => {
             state.isDynamicNetworkSelect = isDynamic;
+          }),
+        setTheme: (theme: ThemeColorType) =>
+          set((state) => {
+            state.theme = theme;
           }),
         resetStoredData: () =>
           set((state) => {
@@ -319,6 +330,10 @@ export const createStore = (options: CreateStoreOptions) =>
             set((state) => {
               state.account.publicKey = publicKey;
               state.account.secretKey = secretKey || "";
+            }),
+          updateWalletKitPubKey: (value?: string) =>
+            set((state) => {
+              state.account.walletKitPubKey = value;
             }),
           reset: () =>
             set((state) => {
