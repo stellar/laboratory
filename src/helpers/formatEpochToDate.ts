7901902c7e607c75142d4321417787493dcf982a
Iveta <quietbits@users.noreply.github.com>
2024-10-23 08:45:37 -0400

    Build Transaction: time bounds timestamp human readable date (#1105)
    * Build Transaction: time bounds timestamp human readable date
    
    * Use UTC timezone

---
+++ src/helpers/formatEpochToDate.ts
@@ -0,0 +1,17 @@
+export const formatEpochToDate = (epoch: number) => {
+  const date = new Date(epoch * 1000);
+  const dateTimeFormatter = new Intl.DateTimeFormat("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hourCycle: "h24",
+    timeZone: "utc",
+    timeZoneName: "short",
+  });
+
+  return dateTimeFormatter.format(date);
+};
