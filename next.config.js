81e0ff9be6b0825438e90cfdd71ba34b7644ba2b
Iveta <quietbits@gmail.com>
2024-10-25 13:57:56 -0400

    Test env vars

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  env: {
+    NEXT_PUBLIC_AMPLITUDE_API_KEY_1:
+      process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY_1,
+    NEXT_PUBLIC_AMPLITUDE_API_KEY_2:
+      process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY_2,
+  },
 };
 
 module.exports = nextConfig;
--- src/components/layout/LayoutMain.tsx
+++ src/components/layout/LayoutMain.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 import { MaintenanceBanner } from "@/components/MaintenanceBanner";
 import { Hydration } from "@/components/Hydration";
 import { LayoutHeader } from "@/components/layout/LayoutHeader";
 
 export const LayoutMain = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    console.log(
+      ">>> process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY_1: ",
+      process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY_1,
+    );
+
+    console.log(
+      ">>> process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY_2: ",
+      process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY_2,
+    );
+  }, []);
+
   return (
     <div className="LabLayout">
       <div>
