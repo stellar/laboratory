890b774e94aa3b03be162b9a60c1a3eb2bd9363c
Iveta <quietbits@gmail.com>
2024-11-04 14:42:24 -0500

    Handle global errors + fix fetch sequence number crash

---
+++ src/app/error.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Button, Card, Heading, Icon, Text } from "@stellar/design-system";
+import { Box } from "@/components/layout/Box";
+import { LayoutContentContainer } from "@/components/layout/LayoutContentContainer";
+import { openUrl } from "@/helpers/openUrl";
+
+export default function Error({
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  // TODO: track this in Sentry once set up
+  console.log("Unhandled error: ", error);
+
+  return (
+    <LayoutContentContainer>
+      <Card>
+        <Box gap="xl" align="start">
+          <Box gap="md">
+            <Heading as="h2" size="xs" weight="medium">
+              Unhandled Error
+            </Heading>
+
+            <Text size="sm" as="p">
+              Uh-oh, we didn’t handle this error. We would appreciate it if you
+              opened an issue on GitHub, providing as many details as possible
+              to help us fix this bug.
+            </Text>
+          </Box>
+
+          <Box gap="md" direction="row">
+            <Button
+              size="sm"
+              variant="secondary"
+              icon={<Icon.ArrowLeft />}
+              iconPosition="left"
+              onClick={() => {
+                location.reload();
+              }}
+            >
+              Return
+            </Button>
+
+            <Button
+              size="sm"
+              variant="primary"
+              iconPosition="left"
+              onClick={() =>
+                openUrl("https://github.com/stellar/laboratory/issues")
+              }
+            >
+              Open Issue
+            </Button>
+          </Box>
+        </Box>
+      </Card>
+    </LayoutContentContainer>
+  );
+}
