e817e12239419fd7867a19c3cf4063ad0cecd7e3
Jeesun Kim <3912060+jeesunikim@users.noreply.github.com>
2025-01-28 10:45:42 -0800

    update getContractIdError

--- src/validate/methods/getContractIdError.ts
+++ src/validate/methods/getContractIdError.ts
@@ -1,10 +1,15 @@
-const CONTRACT_LENGTH = 56;
+// contract IDs are fixed 32-byte byte arrays, and are represented as BytesN<32>
+// https://developers.stellar.org/docs/learn/encyclopedia/contract-development/types/built-in-types#bytes-strings-bytes-bytesn-string
+const CONTRACT_MAX_LENGTH = 66;
+const CONTRACT_MIN_LENGTH = 32;
 
 export const getContractIdError = (value: string) => {
   if (value.charAt(0) !== "C") {
     return "The string must start with 'C'.";
-  } else if (value.length !== CONTRACT_LENGTH) {
-    return `The string must be exactly ${CONTRACT_LENGTH} characters long.`;
+  } else if (value.length > CONTRACT_MAX_LENGTH) {
+    return `The string length is too large.`;
+  } else if (value.length < CONTRACT_MIN_LENGTH) {
+    return `The string length should be at least ${CONTRACT_MIN_LENGTH} characters long.`;
   }
 
   return false;
