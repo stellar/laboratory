f3a07d966517962734b6f397c471857968c4974d
Iveta <quietbits@gmail.com>
2025-04-17 14:25:24 -0400

    Next.js 15 + React 19

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   output: "standalone",
   distDir: "build",
   basePath: process.env.NEXT_BASE_PATH || undefined,
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 };
 
 module.exports = nextConfig;
