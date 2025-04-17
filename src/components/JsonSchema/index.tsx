f8280921b77230a7507eee7e1ead86002fc3e73f
Jeesun Kim <3912060+jeesunikim@users.noreply.github.com>
2025-04-17 16:34:31 -0700

    WIP > move resuable invoke contract function to utils

--- src/components/JsonSchema/index.tsx
+++ src/components/JsonSchema/index.tsx
@@ -10,7 +10,7 @@ import { useStore } from "@/store/useStore";
 import { DereferencedSchemaType } from "@/constants/jsonSchema";
 
 import { dereferenceSchema } from "@/helpers/dereferenceSchema";
-import { buildTxWithSorobanData } from "@/helpers/sorobanUtils";
+import { getTxnToSimulate } from "@/helpers/sorobanUtils";
 import { getNetworkHeaders } from "@/helpers/getNetworkHeaders";
 
 import { useRpcPrepareTx } from "@/query/useRpcPrepareTx";
@@ -184,84 +184,84 @@ const renderTitle = (name: string, description: string) => (
   </>
 );
 
-const getTxnToSimulate = (
-  value: SorobanInvokeValue,
-  txnParams: TransactionBuildParams,
-  operation: TxnOperation,
-  networkPassphrase: string,
-): { xdr: string; error: string } => {
-  try {
-    const argsToScVals = getScValsFromArgs(value.args);
-    const builtXdr = buildTxWithSorobanData({
-      params: txnParams,
-      sorobanOp: {
-        ...operation,
-        params: {
-          ...operation.params,
-          contract_id: value.contract_id,
-          function_name: value.function_name,
-          args: argsToScVals,
-          resource_fee: BASE_FEE, // bogus resource fee for simulation purpose
-        },
-      },
-      networkPassphrase,
-    });
+// const getTxnToSimulate = (
+//   value: SorobanInvokeValue,
+//   txnParams: TransactionBuildParams,
+//   operation: TxnOperation,
+//   networkPassphrase: string,
+// ): { xdr: string; error: string } => {
+//   try {
+//     const argsToScVals = getScValsFromArgs(value.args);
 
-    return { xdr: builtXdr.toXDR(), error: "" };
-  } catch (e: any) {
-    return { xdr: "", error: e.message };
-  }
-};
+//       params: txnParams,
+//       sorobanOp: {
+//         ...operation,
+//         params: {
+//           ...operation.params,
+//           contract_id: value.contract_id,
+//           function_name: value.function_name,
+//           args: argsToScVals,
+//           resource_fee: BASE_FEE, // bogus resource fee for simulation purpose
+//         },
+//       },
+//       networkPassphrase,
+//     });
 
-const getScValsFromArgs = (args: SorobanInvokeValue["args"]): xdr.ScVal[] => {
-  const scVals: xdr.ScVal[] = [];
+//     return { xdr: builtXdr.toXDR(), error: "" };
+//   } catch (e: any) {
+//     return { xdr: "", error: e.message };
+//   }
+// };
 
-  for (const argKey in args) {
-    const argValue = args[argKey];
-    // Note: argValue is either an object or array of objects
-    if (Array.isArray(argValue)) {
-      const arrayScVals = argValue.map((v) => {
-        const convertedValue: Record<string, any> = {};
-        const typeHints: Record<string, [string, string]> = {};
+// const getScValsFromArgs = (args: SorobanInvokeValue["args"]): xdr.ScVal[] => {
+//   const scVals: xdr.ScVal[] = [];
 
-        for (const key in v) {
-          convertedValue[key] = v[key].value;
-          // toLowerCase() is needed because the type we save from the label is in uppercase
-          // but nativeToScval expects the type to be in lowercase
-          typeHints[key] = ["symbol", v[key].type.toLowerCase()];
-        }
+//   for (const argKey in args) {
+//     const argValue = args[argKey];
+//     // Note: argValue is either an object or array of objects
+//     if (Array.isArray(argValue)) {
+//       const arrayScVals = argValue.map((v) => {
+//         const convertedValue: Record<string, any> = {};
+//         const typeHints: Record<string, [string, string]> = {};
 
-        // for an array of objects, `nativeToScVal` expects the following type for val:
-        //   {
-        //     "address": "CDVQVKOY2YSXS2IC7KN6MNASSHPAO7UN2UR2ON4OI2SKMFJNVAMDX6DP",
-        //     "amount": "2",
-        //     "request_type": "2"
-        // }
-        // for `type`, it expects the following type:
-        //   {
-        //     "address": [
-        //         "symbol", // matching the key type
-        //         "address" // matching the value type
-        //     ],
-        //     "amount": [
-        //         "symbol", // matching the key type
-        //         "i128" // matching the value type
-        //     ],
-        //     "request_type": [
-        //         "symbol", // matching the key type
-        //         "u32" // matching the value type
-        //     ]
-        // }
-        return nativeToScVal(convertedValue, { type: typeHints });
-      });
+//         for (const key in v) {
+//           convertedValue[key] = v[key].value;
+//           // toLowerCase() is needed because the type we save from the label is in uppercase
+//           // but nativeToScval expects the type to be in lowercase
+//           typeHints[key] = ["symbol", v[key].type.toLowerCase()];
+//         }
 
-      scVals.push(...arrayScVals);
-    } else {
-      scVals.push(
-        nativeToScVal(argValue.value, { type: argValue.type.toLowerCase() }),
-      );
-    }
-  }
+//         // for an array of objects, `nativeToScVal` expects the following type for val:
+//         //   {
+//         //     "address": "CDVQVKOY2YSXS2IC7KN6MNASSHPAO7UN2UR2ON4OI2SKMFJNVAMDX6DP",
+//         //     "amount": "2",
+//         //     "request_type": "2"
+//         // }
+//         // for `type`, it expects the following type:
+//         //   {
+//         //     "address": [
+//         //         "symbol", // matching the key type
+//         //         "address" // matching the value type
+//         //     ],
+//         //     "amount": [
+//         //         "symbol", // matching the key type
+//         //         "i128" // matching the value type
+//         //     ],
+//         //     "request_type": [
+//         //         "symbol", // matching the key type
+//         //         "u32" // matching the value type
+//         //     ]
+//         // }
+//         return nativeToScVal(convertedValue, { type: typeHints });
+//       });
 
-  return scVals;
-};
+//       scVals.push(...arrayScVals);
+//     } else {
+//       scVals.push(
+//         nativeToScVal(argValue.value, { type: argValue.type.toLowerCase() }),
+//       );
+//     }
+//   }
+
+//   return scVals;
+// };
