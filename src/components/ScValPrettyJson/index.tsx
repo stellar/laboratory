8aacacb8246555199ff2f81d61f0defd41136fa4
Iveta <quietbits@gmail.com>
2025-05-20 13:27:40 -0400

    Refactor ScValPrettyJson to add types

--- src/components/ScValPrettyJson/index.tsx
+++ src/components/ScValPrettyJson/index.tsx
@@ -1,23 +1,22 @@
 import { Fragment } from "react";
-import { XdrLargeInt } from "@stellar/stellar-sdk";
 import { parse, stringify } from "lossless-json";
 import { v4 as uuidv4 } from "uuid";
-
-import { SdsLink } from "@/components/SdsLink";
-import { STELLAR_EXPERT } from "@/constants/settings";
+import { XdrLargeInt } from "@stellar/stellar-sdk";
 
 import { useStore } from "@/store/useStore";
-import { shortenStellarAddress } from "@/helpers/shortenStellarAddress";
+import { getContractIdError } from "@/validate/methods/getContractIdError";
+import { STELLAR_EXPERT } from "@/constants/settings";
+import { SdsLink } from "@/components/SdsLink";
+
 import * as StellarXdr from "@/helpers/StellarXdr";
 import { getStellarExpertNetwork } from "@/helpers/getStellarExpertNetwork";
 import { buildContractExplorerHref } from "@/helpers/buildContractExplorerHref";
-import { getContractIdError } from "@/validate/methods/getContractIdError";
+import { shortenStellarAddress } from "@/helpers/shortenStellarAddress";
 
 import { AnyObject } from "@/types/types";
 
 import "./styles.scss";
 
-/* Create contract storage JSON-like structure */
 export const ScValPrettyJson = ({
   isReady,
   xdrString,
@@ -44,13 +43,7 @@ export const ScValPrettyJson = ({
     }
   };
 
-  type ItemType =
-    | "array"
-    | "object"
-    | "function"
-    | "primitive"
-    | "boolean"
-    | null;
+  type ItemType = "array" | "object" | "function" | "primitive" | null;
 
   const getItemType = (item: any): ItemType => {
     if (typeof item === "object") {
@@ -79,269 +72,322 @@ export const ScValPrettyJson = ({
           };
 
       return (
-        <div className="ScValPrettyJson__value" data-type="address">
-          <SdsLink {...params} variant="secondary" isUnderline title={value}>
-            {shortenStellarAddress(value)}
-          </SdsLink>
-        </div>
+        <SdsLink {...params} variant="secondary" isUnderline title={value}>
+          {shortenStellarAddress(value)}
+        </SdsLink>
+      );
+    }
+
+    return shortenStellarAddress(value);
+  };
+
+  const renderPrimitive = ({
+    value,
+    type,
+    isValueOnly,
+    hasComma = false,
+  }: {
+    value: any;
+    type: string;
+    isValueOnly?: boolean;
+    hasComma?: boolean;
+  }) => {
+    switch (type) {
+      case "address":
+        return (
+          <Value key={`${type}-${uuidv4()}`} type={type} hasComma={hasComma}>
+            {renderAddress(value)}
+          </Value>
+        );
+      case "i8":
+      case "u8":
+      case "i16":
+      case "u16":
+      case "i32":
+      case "u32":
+      case "i64":
+      case "u64":
+        return isValueOnly ? (
+          stringify(value)
+        ) : (
+          <Value key={`${type}-${uuidv4()}`} type={type} hasComma={hasComma}>
+            {stringify(value)}
+          </Value>
+        );
+      case "i128":
+      case "u128":
+      case "i256":
+      case "u256": {
+        let val = stringify(value);
+
+        try {
+          const hiVal = stringify((value as any)?.hi);
+          const loVal = stringify((value as any)?.lo);
+
+          if (hiVal && loVal) {
+            val = stringify(new XdrLargeInt(type, [loVal, hiVal]).toBigInt());
+          }
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        } catch (e) {
+          // do nothing
+        }
+
+        return isValueOnly ? (
+          val
+        ) : (
+          <Value key={`${type}-${uuidv4()}`} type={type} hasComma={hasComma}>
+            {val}
+          </Value>
+        );
+      }
+      case "symbol":
+        if (typeof value === "object") {
+          return render({ item: value, parentType: type, hasComma });
+        }
+
+        if (isValueOnly) {
+          return value;
+        }
+
+        return (
+          <Value key={`${type}-${uuidv4()}`} type={type} hasComma={hasComma}>
+            {value}
+          </Value>
+        );
+      case "bool":
+      case "string":
+      case "wasm":
+      default:
+        return isValueOnly ? (
+          value
+        ) : (
+          <Value key={`${type}-${uuidv4()}`} type={type} hasComma={hasComma}>
+            {value}
+          </Value>
+        );
+    }
+  };
+
+  const renderObject = (obj: AnyObject) => {
+    const objItems = Object.entries(obj);
+
+    return (
+      <Fragment key={`obj-${uuidv4()}`}>
+        <Bracket char="{" />
+        <Block>
+          {objItems.map((objItm: any, idx: number) => {
+            const keyValue = objItm[0];
+            let valType = "";
+            let valValue = objItm[1];
+
+            if (!Array.isArray(valValue)) {
+              [valType, valValue] = Object.entries(objItm[1])[0];
+            }
+
+            return (
+              <Row key={`${valType}-${uuidv4()}`}>
+                <Key hasColon={true}>{keyValue}</Key>
+                {renderObjectOrPrimitive({
+                  value: valValue,
+                  type: valType,
+                  hasComma: idx !== objItems.length - 1,
+                })}
+              </Row>
+            );
+          })}
+        </Block>
+        <Bracket char="}" />
+      </Fragment>
+    );
+  };
+
+  const renderArray = (
+    arrItems: any[],
+    type: string,
+    hasComma: boolean = false,
+  ) => {
+    const isMap = type === "map";
+
+    if (arrItems.length === 0) {
+      return isMap ? (
+        <EmptyObject hasComma={hasComma} />
+      ) : (
+        <EmptyArray hasComma={hasComma} />
       );
     }
 
     return (
-      <div className="ScValPrettyJson__value" data-type="address" title={value}>
-        {shortenStellarAddress(value)}
-      </div>
+      <Fragment key={`${type}-${uuidv4()}`}>
+        <Bracket char={isMap ? "{" : "["} />
+        <Block>
+          {arrItems.map((itm: any, idx: number) =>
+            renderObjectOrPrimitive({
+              value: itm,
+              type,
+              hasComma: idx !== arrItems.length - 1,
+            }),
+          )}
+        </Block>
+        <Bracket char={isMap ? "}" : "]"} />
+        {/* Comma for nested items */}
+        {hasComma ? <Comma /> : null}
+      </Fragment>
+    );
+  };
+
+  const renderKey = (keyType: string, keyValue: any) => {
+    // Key type can be a vector, handling it here
+    if (keyType === "vec") {
+      const [vecType, vecVal] = Object.entries(keyValue[0])[0];
+
+      return (
+        <Key type={vecType} hasColon={true}>
+          {renderPrimitive({
+            value: vecVal,
+            type: vecType,
+            isValueOnly: true,
+          })}
+        </Key>
+      );
+    }
+
+    return (
+      <Key type={keyType} hasColon={true}>
+        {renderPrimitive({
+          value: keyValue,
+          type: keyType,
+          isValueOnly: true,
+        })}
+      </Key>
+    );
+  };
+
+  const renderKeyValue = ({
+    mapItem,
+    hasComma = false,
+  }: {
+    mapItem: any;
+    hasComma?: boolean;
+  }) => {
+    const [keyType, keyValue] = Object.entries(mapItem.key)[0];
+    const [valType, valValue] = Object.entries(mapItem.val)[0];
+
+    return (
+      <Row key={`${valType}-${valType}-${uuidv4()}`}>
+        {renderKey(keyType, keyValue)}
+        {renderObjectOrPrimitive({ value: valValue, type: valType, hasComma })}
+      </Row>
     );
   };
 
   const render = ({
     item,
-    parentKey,
-    isKey,
-    renderKey,
+    parentType,
+    hasComma = false,
   }: {
     item: any;
-    parentKey?: string;
-    isKey?: boolean;
-    renderKey?: string;
+    parentType?: string;
+    hasComma?: boolean;
   }): React.ReactNode => {
     const itemType = getItemType(item);
-    // "map" array should be rendered as object
-    const isMap = parentKey === "map";
 
+    // Array
     if (itemType === "array") {
+      // "map" array should be rendered as object
+      const isMap = parentType === "map";
+
       // Empty array or object
       if (item.length === 0) {
-        return (
-          <div className="ScValPrettyJson__value" key={renderKey}>
-            {isMap ? "{ }" : "[ ]"}
-          </div>
-        );
+        return isMap ? <EmptyObject /> : <EmptyArray />;
       }
 
-      const renderedItems = item.map((arrayItem: any, arrayIndex: number) => (
-        <Fragment key={`${renderKey}-${arrayIndex}`}>
-          {render({
-            item: arrayItem,
-            parentKey,
-            isKey,
-            renderKey: `${renderKey || ""}${parentKey || ""}-${arrayIndex}`,
-          })}
-        </Fragment>
-      ));
-
-      if (isKey) {
-        return renderedItems;
-      }
-
-      return (
-        <Fragment key={renderKey}>
-          <Bracket char={isMap ? "{" : "["} />
-          <div className="ScValPrettyJson__container">{renderedItems}</div>
-          <Bracket char={isMap ? "}" : "]"} />
-        </Fragment>
-      );
+      return renderArray(item, "");
     }
 
+    // Object
     if (itemType === "object") {
-      const entries = Object.entries(item);
+      const scType: string = Object.keys(item)[0];
+      const scValue: any = Object.values(item)[0];
 
-      if (entries.length === 2) {
-        // Handle key/value pair
-        const [key, keyVal] = entries[0];
-        const [value, valueVal] = entries[1];
-
-        if (key === "key" && value === "val") {
+      switch (scType) {
+        case "contract_instance":
+          return renderObject(scValue);
+        case "map":
           return (
-            <div className="ScValPrettyJson__row" key={renderKey}>
-              {render({
-                item: keyVal,
-                parentKey,
-                isKey: true,
-                renderKey: `${renderKey}-${parentKey}-key`,
-              })}
-              {render({
-                item: valueVal,
-                parentKey,
-                isKey: false,
-                renderKey: `${renderKey}-${parentKey}-val`,
-              })}
-            </div>
+            <Fragment key={`${scType}-${uuidv4()}`}>
+              <Bracket char="{" />
+              <Block>
+                {scValue.map((mapItm: any, idx: number) =>
+                  renderKeyValue({
+                    mapItem: mapItm,
+                    hasComma: idx !== scValue.length - 1,
+                  }),
+                )}
+              </Block>
+              <Bracket char="}" />
+            </Fragment>
           );
-        }
+        case "vec":
+          return renderArray(scValue, scType);
+        default:
+          return renderPrimitive({
+            value: scValue,
+            type: scType,
+            hasComma,
+          });
       }
-
-      // Render by object key type
-      if (entries.length === 1) {
-        const [eKey, eVal] = entries[0];
-
-        switch (eKey) {
-          case "contract_instance":
-          case "map":
-          case "vec":
-          case "wasm":
-          case "symbol":
-          case "string":
-            return render({
-              item: eVal,
-              parentKey: eKey,
-              isKey,
-              // Using UUID to make sure key will be unique
-              renderKey: `${renderKey || parentKey || ""}${eKey}-${uuidv4()}`,
-            });
-          case "bool":
-            return (
-              <div
-                className="ScValPrettyJson__value"
-                data-type={eKey}
-                key={renderKey}
-              >
-                {stringify(eVal)}
-              </div>
-            );
-          case "address":
-            return renderAddress(eVal as string);
-          case "i8":
-          case "u8":
-          case "i16":
-          case "u16":
-          case "i32":
-          case "u32":
-          case "i64":
-          case "u64":
-            return render({
-              item: stringify(eVal),
-              parentKey: eKey,
-              isKey,
-              renderKey: `${renderKey}-${parentKey}-${eKey}`,
-            });
-          case "i128":
-          case "u128":
-          case "i256":
-          case "u256":
-            try {
-              const hiVal = stringify((eVal as any)?.hi);
-              const loVal = stringify((eVal as any)?.lo);
-
-              if (hiVal && loVal) {
-                const val = stringify(
-                  new XdrLargeInt(eKey, [loVal, hiVal]).toBigInt(),
-                );
-
-                return render({
-                  item: val,
-                  parentKey: eKey,
-                  isKey,
-                  renderKey: `${renderKey}-${parentKey}-${eKey}`,
-                });
-              }
-              // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            } catch (e) {
-              // do nothing
-            }
-
-            return render({
-              item: stringify(eVal),
-              parentKey: eKey,
-              isKey,
-              renderKey: `${renderKey}-${parentKey}-${eKey}`,
-            });
-          default:
-            return (
-              <Fragment key={renderKey}>
-                <Bracket char="{" />
-                <div className="ScValPrettyJson__container">
-                  <div className="ScValPrettyJson__row">
-                    <div className="ScValPrettyJson__key">{eKey}</div>
-                    {render({
-                      item: eVal,
-                      parentKey: eKey,
-                      isKey,
-                      renderKey: `${renderKey}-${parentKey}-${eKey}`,
-                    })}
-                  </div>
-                </div>
-                <Bracket char="}" />
-              </Fragment>
-            );
-        }
-      }
-
-      return (
-        <Fragment key={renderKey}>
-          <Bracket char="{" />
-          <div className="ScValPrettyJson__container">
-            {entries.map((e, eIdx) => {
-              const [eKey, eVal] = e;
-              const renKey = `${renderKey}-${parentKey}-${eKey}-${eIdx}`;
-
-              switch (eKey) {
-                // Don't render these keys
-                case "contract_instance":
-                case "vec":
-                case "wasm":
-                case "symbol":
-                  return (
-                    <Fragment key={renKey}>
-                      {render({
-                        item: eVal,
-                        parentKey: eKey,
-                        isKey,
-                        renderKey: renKey,
-                      })}
-                    </Fragment>
-                  );
-                default:
-                  return (
-                    <div className="ScValPrettyJson__row" key={renKey}>
-                      <div className="ScValPrettyJson__key">{eKey}</div>
-                      {render({
-                        item: eVal,
-                        parentKey: eKey,
-                        isKey,
-                        renderKey: renKey,
-                      })}
-                    </div>
-                  );
-              }
-            })}
-          </div>
-          <Bracket char="}" />
-        </Fragment>
-      );
     }
 
-    if (itemType === "function") {
-      return (
-        <div
-          className="ScValPrettyJson__value"
-          data-type="function"
-          key={renderKey}
-        >{`${JSON.stringify(item)}`}</div>
-      );
-    }
-
-    if (itemType === "primitive") {
-      return (
-        <div
-          className={isKey ? "ScValPrettyJson__key" : "ScValPrettyJson__value"}
-          data-type={parentKey}
-          key={renderKey}
-        >
-          {item}
-        </div>
-      );
-    }
-
-    // If the item is null
-    return (
-      <div className="ScValPrettyJson__value" key={renderKey}>
-        null
-      </div>
-    );
+    return renderPrimitive({
+      value: item,
+      type: "",
+      hasComma,
+    });
   };
 
-  // Entry point
+  const renderObjectOrPrimitive = ({
+    value,
+    type,
+    hasComma = false,
+  }: {
+    value: any;
+    type: string;
+    hasComma?: boolean;
+  }) => {
+    // If value is an array
+    if (Array.isArray(value)) {
+      return renderArray(value, type, hasComma);
+    }
+
+    let isKeyValue = false;
+
+    // Checking if an object is a key-value object
+    if (typeof value === "object") {
+      const keys = Object.keys(value);
+
+      if (keys.length === 2 && keys[0] === "key" && keys[1] === "val") {
+        isKeyValue = true;
+      }
+    }
+
+    if (isKeyValue) {
+      return renderKeyValue({
+        mapItem: value,
+        hasComma,
+      });
+    }
+
+    // If not a "primitive" value, we need to render as an object
+    const objectTypes = ["contract_instance", "map", "vec"];
+
+    if (type && objectTypes.includes(type)) {
+      return render({ item: value, parentType: type, hasComma });
+    }
+
+    return renderPrimitive({ value, type, hasComma });
+  };
+
+  // Entry point: render JSON, if provided, or parse XDR to JSON
   return (
     <div className="ScValPrettyJson">
       {render({ item: json ? json : parseJson() })}
@@ -352,6 +398,89 @@ export const ScValPrettyJson = ({
 // =============================================================================
 // Components
 // =============================================================================
-const Bracket = ({ char }: { char: "{" | "}" | "[" | "]" }) => {
-  return <div className="ScValPrettyJson__bracket">{char}</div>;
+// On the UI we use "sym" instead of "symbol"
+const getType = (type: string) => (type === "symbol" ? "sym" : type);
+
+const Block = ({ children }: { children: React.ReactNode }) => (
+  <div className="ScValPrettyJson__block">{children}</div>
+);
+
+const Row = ({ children }: { children: React.ReactNode }) => (
+  <div className="ScValPrettyJson__row">{children}</div>
+);
+
+const Key = ({
+  children,
+  type,
+  hasColon,
+}: {
+  children: React.ReactNode;
+  type?: string;
+  hasColon: boolean;
+}) => {
+  const _type = getType(type || "");
+
+  return (
+    <Fragment key={`key-${type}-${uuidv4()}`}>
+      <div className="ScValPrettyJson__key">
+        <div {...(_type ? { "data-type": _type } : {})}>
+          {_type === "sym" ? <Quotes>{children}</Quotes> : children}
+        </div>
+      </div>
+      {hasColon ? <Colon /> : null}
+    </Fragment>
+  );
 };
+
+const Value = ({
+  children,
+  hasComma,
+  type,
+}: {
+  children: React.ReactNode;
+  hasComma?: boolean;
+  type?: string;
+}) => {
+  const _type = getType(type || "");
+  const _children = _type === "bool" ? `${children}` : children;
+  const hasQuotes = ["sym", "string"].includes(_type);
+
+  return (
+    <div className="ScValPrettyJson__value">
+      <div {...(_type ? { "data-type": _type } : {})}>
+        {hasQuotes ? <Quotes>{_children}</Quotes> : _children}
+      </div>
+      {hasComma ? <Comma /> : null}
+    </div>
+  );
+};
+
+const Bracket = ({ char }: { char: "{" | "}" | "[" | "]" }) => (
+  <div className="ScValPrettyJson__bracket">{char}</div>
+);
+
+const Colon = () => <div className="ScValPrettyJson__colon">:</div>;
+
+const Comma = () => <div className="ScValPrettyJson__comma">,</div>;
+
+const Quotes = ({ children }: { children: React.ReactNode }) => (
+  <>
+    <div className="ScValPrettyJson__quotes">{'"'}</div>
+    {children}
+    <div className="ScValPrettyJson__quotes">{'"'}</div>
+  </>
+);
+
+const EmptyObject = ({ hasComma }: { hasComma?: boolean }) => (
+  <Fragment key={`emptyObj-${uuidv4()}`}>
+    <Value>{"{  }"}</Value>
+    {hasComma ? <Comma /> : null}
+  </Fragment>
+);
+
+const EmptyArray = ({ hasComma }: { hasComma?: boolean }) => (
+  <Fragment key={`emptyArr-${uuidv4()}`}>
+    <Value>{"[  ]"}</Value>
+    {hasComma ? <Comma /> : null}
+  </Fragment>
+);
