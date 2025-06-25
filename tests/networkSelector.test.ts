29861f1d3055c239eaa9dca1d28acf331085f2c4
Iveta <quietbits@gmail.com>
2025-06-25 16:33:15 -0400

    Update test

--- tests/networkSelector.test.ts
+++ tests/networkSelector.test.ts
@@ -22,7 +22,7 @@ test.describe("Network selector", () => {
       page
         .getByTestId("networkSelector-dropdown")
         .getByTestId("networkSelector-option"),
-    ).toContainText(["Futurenet", "Testnet", "Mainnet", "Custom"]);
+    ).toContainText(["Testnet", "Mainnet", "Futurenet", "Custom"]);
   });
 
   test("Shows correct network data for the selected network", async ({
