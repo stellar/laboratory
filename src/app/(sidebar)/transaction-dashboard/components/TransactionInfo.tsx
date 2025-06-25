12f48fca1e0eaa4463e51a872f0c1381ca090be4
Iveta <quietbits@gmail.com>
2025-06-25 14:48:34 -0400

    Transaction Dashboard: Fee breakdown

--- src/app/(sidebar)/transaction-dashboard/components/TransactionInfo.tsx
+++ src/app/(sidebar)/transaction-dashboard/components/TransactionInfo.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { Asset, Avatar, Badge, Card, Icon } from "@stellar/design-system";
 import { stringify } from "lossless-json";
+import { BigNumber } from "bignumber.js";
 
 import { Box } from "@/components/layout/Box";
 import { InfoFieldItem } from "@/components/InfoFieldItem";
 import { SdsLink } from "@/components/SdsLink";
 import { NoInfoLoadedView } from "@/components/NoInfoLoadedView";
+import { ExpandBox } from "@/components/ExpandBox";
 
 import { isSorobanOperationType } from "@/helpers/sorobanUtils";
 import { formatEpochToDate } from "@/helpers/formatEpochToDate";
@@ -24,6 +27,8 @@ export const TransactionInfo = ({
   txDetails: RpcTxJsonResponse | null;
 }) => {
   const { network } = useStore();
+
+  const [isFeeBreakdownOpen, setIsFeeBreakdownOpen] = useState(false);
 
   const isDataLoaded = Boolean(txDetails);
   const transaction = txDetails?.envelopeJson?.tx?.tx;
@@ -90,6 +95,51 @@ export const TransactionInfo = ({
     },
   ];
 
+  type FeeBreakdown = {
+    transactionFee: string;
+    refundableFee: string;
+    nonRefundableFee: string;
+    totalResourceFee: string;
+    inclusionFee: string;
+  };
+
+  const getFeeBreakdown = (): FeeBreakdown | null => {
+    const transactionFee = txDetails?.resultJson?.fee_charged;
+
+    if (!transactionFee) {
+      return null;
+    }
+
+    const {
+      total_refundable_resource_fee_charged,
+      total_non_refundable_resource_fee_charged,
+    } = txDetails?.resultMetaJson?.v3?.soroban_meta?.ext?.v1 || {};
+
+    const refundableFee = BigNumber(
+      total_refundable_resource_fee_charged || 0,
+    ).toString();
+
+    const nonRefundableFee = BigNumber(
+      total_non_refundable_resource_fee_charged || 0,
+    ).toString();
+
+    const totalResourceFee = BigNumber(refundableFee)
+      .plus(nonRefundableFee)
+      .toString();
+
+    const inclusionFee = BigNumber(transactionFee)
+      .minus(totalResourceFee)
+      .toString();
+
+    return {
+      transactionFee,
+      refundableFee,
+      nonRefundableFee,
+      totalResourceFee,
+      inclusionFee,
+    };
+  };
+
   const formatData = () => {
     if (!txDetails || !transaction) {
       return null;
@@ -118,6 +168,7 @@ export const TransactionInfo = ({
       ...baseProps,
       ...(IS_SOROBAN_TX ? {} : classicTxProps),
       ...feeProps,
+      ...(IS_SOROBAN_TX ? { feeBreakdown: getFeeBreakdown() } : {}),
     };
   };
 
@@ -167,31 +218,136 @@ export const TransactionInfo = ({
     );
   };
 
-  const renderFee = (fee: any) => {
+  const renderFee = (
+    fee: string | number,
+    badgeVariant: "tertiary" | "secondary" = "tertiary",
+  ) => {
     if (!fee) {
       return null;
     }
 
-    const stroops = stringify(fee);
+    const stroops = fee;
 
     if (!stroops) {
       return null;
     }
 
     return (
-      <Box gap="sm" direction="row" align="center">
+      <Box gap="sm" direction="row" align="center" wrap="wrap">
         <Box gap="xs" direction="row" align="center">
-          {stroopsToLumens(stroops)} XLM{" "}
+          {stroopsToLumens(stroops.toString())} XLM{" "}
           <Asset
             variant="single"
             size="sm"
             sourceOne={{ image: STELLAR_LOGO_DATA_URI, altText: "XLM logo" }}
           />
         </Box>
-        <Badge variant="tertiary" size="sm">
-          {`${formatNumber(fee)} stroops`}
+        <Badge variant={badgeVariant} size="sm">
+          {`${formatNumber(BigInt(fee))} stroops`}
         </Badge>
       </Box>
+    );
+  };
+
+  const FeeBreakdownItem = ({
+    label,
+    fee,
+    type,
+    details,
+    isCenterAligned = false,
+  }: {
+    label: string;
+    fee: string;
+    type: "solid" | "dotted";
+    details?: string[];
+    isCenterAligned?: boolean;
+  }) => {
+    return (
+      <Box
+        gap="xs"
+        addlClassName="TransactionInfo__feeBreakdown__item"
+        data-type={type}
+        data-center-aligned={isCenterAligned}
+      >
+        <Box
+          gap="xs"
+          direction="row"
+          align="center"
+          justify={type === "solid" ? "center" : "left"}
+          wrap="wrap"
+        >
+          <div className="TransactionInfo__feeBreakdown__item__label">
+            {label}:
+          </div>
+          <div className="TransactionInfo__feeBreakdown__item__value">
+            {renderFee(fee, type === "solid" ? "secondary" : "tertiary")}
+          </div>
+        </Box>
+
+        {details ? (
+          <div>
+            {details.map((d, index) => (
+              <div key={`${d.toLowerCase()}-${index}`}>{d}</div>
+            ))}
+          </div>
+        ) : null}
+      </Box>
+    );
+  };
+
+  const renderFeeBreakdown = (feeBreakdown: FeeBreakdown) => {
+    // TODO: add Inclusion and Resource fees info
+    return (
+      <div className="TransactionInfo__feeBreakdown">
+        {/* Row 1 */}
+        <div className="TransactionInfo__feeBreakdown__row" data-row="1">
+          <FeeBreakdownItem
+            label="Transaction Fee"
+            fee={feeBreakdown.transactionFee}
+            type="solid"
+          />
+        </div>
+
+        {/* Row 2 */}
+        <div className="TransactionInfo__feeBreakdown__row" data-row="2">
+          <FeeBreakdownItem
+            label="Total Resource Fee"
+            fee={feeBreakdown.totalResourceFee}
+            type="solid"
+          />
+
+          <FeeBreakdownItem
+            label="Inclusion Fee"
+            fee={feeBreakdown.inclusionFee}
+            type="dotted"
+            isCenterAligned={true}
+          />
+        </div>
+
+        {/* Row 3 */}
+        <div className="TransactionInfo__feeBreakdown__row" data-row="3">
+          <FeeBreakdownItem
+            label="Refundable"
+            fee={feeBreakdown.refundableFee}
+            type="dotted"
+            details={["Instructions", "Read", "Write", "Bandwidth"]}
+          />
+
+          <FeeBreakdownItem
+            label="Non-refundable"
+            fee={feeBreakdown.nonRefundableFee}
+            type="dotted"
+            details={["Rent", "Events", "Return Value"]}
+          />
+
+          <FeeBreakdownItem
+            label="Inclusion Fee"
+            fee={feeBreakdown.inclusionFee}
+            type="dotted"
+            isCenterAligned={true}
+          />
+        </div>
+      </div>
     );
   };
 
@@ -301,17 +457,48 @@ export const TransactionInfo = ({
             isValueLoaded={isDataLoaded}
             label={field.label}
             value={renderFee(formattedData?.maxFee)}
+            infoText="Max Fee specified in the transaction itself – the maximum XLM amount the source account willing to pay. Each transaction sets a fee that is paid by the source account."
           />
         );
       case "transaction-fee":
         return (
-          <InfoFieldItem
-            key={`tx-info-${field.id}`}
-            isValueLoaded={isDataLoaded}
-            label={field.label}
-            value={renderFee(formattedData?.transactionFee)}
-          />
-          // TODO: Add fee breakdown
+          <Box
+            gap="lg"
+            key={`tx-info-${field.id}-wrapper`}
+            addlClassName="TransactionInfo__txFeeWrapper"
+            data-open={isFeeBreakdownOpen}
+          >
+            <InfoFieldItem
+              key={`tx-info-${field.id}`}
+              isValueLoaded={isDataLoaded}
+              label={field.label}
+              value={
+                <Box gap="sm" direction="row" align="center" wrap="wrap">
+                  {renderFee(formattedData?.transactionFee)}{" "}
+                  {formattedData?.feeBreakdown ? (
+                    <SdsLink
+                      href=""
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setIsFeeBreakdownOpen(!isFeeBreakdownOpen);
+                      }}
+                      icon={<Icon.ChevronRight />}
+                      addlClassName="TransactionInfo__feeBreakdownLink"
+                    >
+                      See fee breakdown
+                    </SdsLink>
+                  ) : null}
+                </Box>
+              }
+              infoText="Transaction Fee is what is actually charged during the transaction execution, which can be lower than the Max Fee specified. (Transaction Fee  = Resource Fee + Inclusion Fee)."
+            />
+
+            {formattedData?.feeBreakdown ? (
+              <ExpandBox offsetTop="lg" isExpanded={isFeeBreakdownOpen}>
+                {renderFeeBreakdown(formattedData.feeBreakdown)}
+              </ExpandBox>
+            ) : null}
+          </Box>
         );
       default:
         return null;
--- src/components/InfoFieldItem/index.tsx
+++ src/components/InfoFieldItem/index.tsx
@@ -1,18 +1,27 @@
 import { Box } from "@/components/layout/Box";
+import { WithInfoText } from "@/components/WithInfoText";
 import "./styles.scss";
 
 export const InfoFieldItem = ({
   label,
   value,
   isValueLoaded,
+  infoText,
 }: {
   label: string;
   value: React.ReactNode;
   isValueLoaded: boolean;
+  infoText?: React.ReactNode;
 }) => {
   return (
     <Box gap="xs" direction="row" align="center" addlClassName="InfoFieldItem">
-      <div className="InfoFieldItem__label">{label}</div>
+      <div className="InfoFieldItem__label">
+        {infoText ? (
+          <WithInfoText infoText={infoText}>{label}</WithInfoText>
+        ) : (
+          label
+        )}
+      </div>
       {isValueLoaded ? (
         <div className="InfoFieldItem__value">{value ?? "-"}</div>
       ) : null}
