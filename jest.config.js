740fa8bcfed331711ca2c01787aaa38536c36ecd
Jeesun Kim <3912060+jeesunikim@users.noreply.github.com>
2025-07-01 21:00:09 -0700

    root to be just '.'

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "ts-jest",
-  roots: ["./src"],
+  roots: ["."],
   collectCoverageFrom: ["src/**/*.ts"],
   testEnvironment: "node",
   transform: { "\\.[jt]sx?$": ["ts-jest", { useESM: true }] },
