5da39a35a6287ccf110d8aaf9d286139c515c433
Iveta <quietbits@users.noreply.github.com>
2025-07-11 14:41:29 -0400

    Update trim() helper method (#1492)

--- src/helpers/trim.ts
+++ src/helpers/trim.ts
@@ -1,3 +1,16 @@
 // Regex explained: https://regexr.com/4v6jg
-export const trim = (str: string, c = "\\s") =>
-  str.replace(new RegExp(`^([${c}]*)(.*?)([${c}]*)$`), "$2");
+export const trim = (str: string, c: string = "\\s") => {
+  // Only allow safe, alphanumeric characters or \s for whitespace.
+  if (!/^[a-zA-Z0-9\s]$/.test(c) && c !== "\\s") {
+    throw new Error("Invalid trim character");
+  }
+  // Use a hardcoded regex with validated character.
+  const pattern =
+    c === "\\s"
+      ? // whitespace trim
+        /^\s*([\s\S]*?)\s*$/
+      : // single character trim
+        new RegExp(`^${c}*([\\s\\S]*?)${c}*$`);
+
+  return str.replace(pattern, "$1");
+};
