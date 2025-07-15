c63ce45e6b35d51b148c32ac175bda50a4992738
Iveta <quietbits@users.noreply.github.com>
2025-07-15 13:53:37 -0400

    Create Keypair: Show mainnet banner only on mainnet (#1501)

--- src/hooks/useIsTestingNetwork.ts
+++ src/hooks/useIsTestingNetwork.ts
@@ -1,7 +1,11 @@
+import { Networks } from "@stellar/stellar-sdk";
 import { useStore } from "@/store/useStore";
 
 export const useIsTestingNetwork = (): boolean => {
   const { network } = useStore();
 
-  return network.id === "testnet" || network.id === "futurenet";
+  return (
+    ["testnet", "futurenet", "custom"].includes(network.id) &&
+    network.passphrase !== Networks.PUBLIC
+  );
 };
