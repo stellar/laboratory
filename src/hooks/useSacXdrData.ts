fe8019635c1f0a7bcdf688dc9b61962b54fdef0a
jeesun 지선 <jeesunikim@users.noreply.github.com>
2025-07-28 10:58:27 -0700

    [Invoke Contract] Enable Stellar Asset Contract (#1516)

---
+++ src/hooks/useSacXdrData.ts
@@ -0,0 +1,59 @@
+import { parse, stringify } from "lossless-json";
+
+import { useGitHubFile } from "@/query/useGitHubFile";
+
+import * as StellarXdr from "@/helpers/StellarXdr";
+
+import { useIsXdrInit } from "@/hooks/useIsXdrInit";
+
+import { STELLAR_ASSET_CONTRACT } from "@/constants/stellarAssetContractData";
+
+import { AnyObject } from "@/types/types";
+
+export const useSacXdrData = ({ isActive }: { isActive: boolean }) => {
+  const {
+    data: sacData,
+    error: sacDataError,
+    isFetching: isSacDataFetching,
+    isLoading: isSacDataLoading,
+  } = useGitHubFile({
+    repo: STELLAR_ASSET_CONTRACT.contractSpecRepo,
+    path: STELLAR_ASSET_CONTRACT.contractSpecPath,
+    file: `${STELLAR_ASSET_CONTRACT.contractSpecFileName}.json`,
+    isActive: Boolean(isActive),
+  });
+
+  const isXdrInit = useIsXdrInit();
+
+  if (!sacData) {
+    return {
+      SacXdrData: [""],
+      sacDataError: null,
+      isSacDataFetching: false,
+      isSacDataLoading: false,
+    };
+  }
+
+  const sacXdrData = (dataString: string | null | undefined): string[] => {
+    if (!dataString) {
+      return [""];
+    }
+
+    const jsonData = parse(dataString) as AnyObject[];
+
+    return isXdrInit
+      ? jsonData.map((d) =>
+          StellarXdr.encode("ScSpecEntry", stringify(d) || ""),
+        )
+      : [""];
+  };
+
+  const sacXdrDataFormatted = sacXdrData(sacData);
+
+  return {
+    sacXdrData: sacXdrDataFormatted,
+    sacDataError: sacDataError,
+    isSacDataFetching: isSacDataFetching,
+    isSacDataLoading: isSacDataLoading,
+  };
+};
