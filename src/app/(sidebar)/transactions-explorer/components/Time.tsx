dd833191f50b264c8e25b075e83b9ed1c87e42f5
Iveta <quietbits@users.noreply.github.com>
2025-08-15 09:03:20 -0400

    Transaction Explorer: UI tweaks (#1539)
    * Transaction Explorer: UI tweaks
    
    * Add ellipsis
    
    * Fix tx restore footprint op test

--- src/app/(sidebar)/transactions-explorer/components/Time.tsx
+++ src/app/(sidebar)/transactions-explorer/components/Time.tsx
@@ -3,5 +3,12 @@ import { formatTimestamp } from "@/helpers/formatTimestamp";
 export function Time({ timestamp }: { timestamp: number }) {
   timestamp = timestamp * 1000;
   const date = new Date(timestamp);
-  return <time title={date.toISOString()}>{formatTimestamp(timestamp)}</time>;
+  return (
+    <time
+      title={date.toISOString()}
+      className="TransactionsExplorer__table__cell"
+    >
+      {formatTimestamp(timestamp)}
+    </time>
+  );
 }
