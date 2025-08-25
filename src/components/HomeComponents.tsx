9b89844b8b9ea905863cb43b8ed69fe2fffdbdb6
Iveta <quietbits@gmail.com>
2025-08-25 13:36:17 -0400

    Updated Resources section

--- src/components/HomeComponents.tsx
+++ src/components/HomeComponents.tsx
@@ -7,6 +7,7 @@ import {
   Button,
   Modal,
   Display,
+  Logo,
 } from "@stellar/design-system";
 
 import { Box } from "@/components/layout/Box";
@@ -806,41 +807,64 @@ export const HomeNetworks = ({
 export const HomeResources = () => {
   const resourceItems = [
     {
-      title: "Developer Docs",
-      description: "Place to find answers, how to build features step by step.",
-      link: "https://developers.stellar.org/",
-    },
-    {
+      id: "youtube",
       title: "YouTube",
       description: "Watch tutorials, hear from us, and get inspired to build.",
       link: "https://www.youtube.com/@StellarDevelopmentFoundation",
+      icon: <Logo.Youtube />,
     },
     {
-      title: "Stellar Quest",
-      description: "Get tested and earn rewards like XLM and NFTs.",
-      link: "https://quest.stellar.org/",
-    },
-    {
-      title: "X (Twitter)",
-      description:
-        "Follow along with us @StellarOrg for updates on our platforms.",
-      link: "https://twitter.com/StellarOrg",
-    },
-    {
+      id: "discord",
       title: "Discord",
       description:
         "Join the conversation and build with us in the dev community.",
       link: "https://discord.gg/stellardev",
+      icon: <Logo.Discord />,
+    },
+    {
+      id: "stellar-quest",
+      title: "Stellar Quest",
+      description: "Get tested and earn rewards like XLM and NFTs.",
+      link: "https://quest.stellar.org/",
+      icon: <Logo.StellarShort />,
+    },
+    {
+      id: "x",
+      title: "X (Twitter)",
+      description:
+        "Follow along with us @StellarOrg for updates on our platforms.",
+      link: "https://twitter.com/StellarOrg",
+      icon: <Logo.XTwitter />,
+    },
+    {
+      id: "dev-docs",
+      title: "Developer Docs",
+      description: "Place to find answers, how to build features step by step.",
+      link: "https://developers.stellar.org/",
+      icon: <Icon.Code01 />,
     },
   ];
 
   return (
     <div className="Lab__home__resources">
-      {resourceItems.map((i, idx) => (
-        <div className="Lab__home__resources__item" key={`resourceItem-${idx}`}>
-          <Text size="sm" as="div" addlClassName="Lab__home__resources__title">
-            {i.title}
-          </Text>
+      {resourceItems.map((i) => (
+        <Box
+          gap="lg"
+          addlClassName="Lab__home__resources__item"
+          data-item={i.id}
+          key={`resourceItem-${i.id}`}
+        >
+          <Box
+            gap="sm"
+            direction="row"
+            align="center"
+            addlClassName="Lab__home__resources__title"
+          >
+            {i.icon}
+            <Text size="sm" as="div">
+              {i.title}
+            </Text>
+          </Box>
           <Text
             size="md"
             as="div"
@@ -848,13 +872,17 @@ export const HomeResources = () => {
             addlClassName="Lab__home__resources__description"
           >
             {i.description}{" "}
-            <SdsLink
-              href={i.link}
-              variant="primary"
-              icon={<Icon.LinkExternal01 />}
-            ></SdsLink>
           </Text>
-        </div>
+
+          <Button
+            size="md"
+            variant="tertiary"
+            icon={<Icon.LinkExternal01 />}
+            onClick={() => openUrl(i.link)}
+          >
+            Check it out
+          </Button>
+        </Box>
       ))}
     </div>
   );
