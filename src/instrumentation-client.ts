0cf20cc75e60426939730efc3650a54ba9fdb877
Iveta <quietbits@gmail.com>
2025-08-28 15:23:10 -0400

    Run Sentry only on lab.stellar.org

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,14 +3,17 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { isSentryEnabled } from "@/constants/settings";
 
-Sentry.init({
-  dsn: "https://75c875528a113643f9a5f74899b53516@o14203.ingest.us.sentry.io/4508100110450688",
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+if (isSentryEnabled) {
+  Sentry.init({
+    dsn: "https://75c875528a113643f9a5f74899b53516@o14203.ingest.us.sentry.io/4508100110450688",
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,15 @@
 import * as Sentry from "@sentry/nextjs";
+import { isSentryEnabled } from "@/constants/settings";
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("../sentry.server.config");
-  }
+  if (isSentryEnabled) {
+    if (process.env.NEXT_RUNTIME === "nodejs") {
+      await import("../sentry.server.config");
+    }
 
-  if (process.env.NEXT_RUNTIME === "edge") {
-    await import("../sentry.edge.config");
+    if (process.env.NEXT_RUNTIME === "edge") {
+      await import("../sentry.edge.config");
+    }
   }
 }
 
