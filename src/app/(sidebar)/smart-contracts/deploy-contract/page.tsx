d7eac10f640c36bd2eef5cfe0dfba52ac4fca285
Iveta <quietbits@gmail.com>
2025-09-08 09:45:35 -0400

    Create deploy contract page

---
+++ src/app/(sidebar)/smart-contracts/deploy-contract/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Box } from "@/components/layout/Box";
+import { PageCard } from "@/components/layout/PageCard";
+
+export default function DeployContract() {
+  return (
+    <Box gap="lg">
+      <PageCard heading="Contract Explorer">TODO: content</PageCard>
+    </Box>
+  );
+}
