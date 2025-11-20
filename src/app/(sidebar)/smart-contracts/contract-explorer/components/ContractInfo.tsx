820361c8ea670c6e2109ebb47965ea54ea76cf4f
jeesun 지선 <jeesunikim@users.noreply.github.com>
2025-11-20 13:29:14 -0800

    extract renderVerifiedBadge into its own component (#1720)

--- src/app/(sidebar)/smart-contracts/contract-explorer/components/ContractInfo.tsx
+++ src/app/(sidebar)/smart-contracts/contract-explorer/components/ContractInfo.tsx
@@ -1,7 +1,6 @@
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { useState } from "react";
 import {
   Avatar,
-  Badge,
   Card,
   Icon,
   Link,
@@ -16,6 +15,7 @@ import { SdsLink } from "@/components/SdsLink";
 import { TabView } from "@/components/TabView";
 import { InfoFieldItem } from "@/components/InfoFieldItem";
 import { NoInfoLoadedView } from "@/components/NoInfoLoadedView";
+import { BuildVerifiedBadge } from "@/components/BuildVerifiedBadge";
 
 import { formatEpochToDate } from "@/helpers/formatEpochToDate";
 import { formatNumber } from "@/helpers/formatNumber";
@@ -64,9 +64,6 @@ export const ContractInfo = ({
   const [activeTab, setActiveTab] = useState<ContractTabId>(
     "contract-contract-spec",
   );
-  const [isBadgeTooltipVisible, setIsBadgeTooltipVisible] = useState(false);
-
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
 
   const isDataLoaded = Boolean(infoData);
 
@@ -82,27 +79,6 @@ export const ContractInfo = ({
   });
 
   const { sourceRepo, sourceCommit } = getRepoData();
-
-  const handleClickOutside = useCallback((event: MouseEvent) => {
-    if (buttonRef?.current?.contains(event.target as Node)) {
-      return;
-    }
-
-    setIsBadgeTooltipVisible(false);
-  }, []);
-
-  // Close tooltip when clicked outside
-  useLayoutEffect(() => {
-    if (isBadgeTooltipVisible) {
-      document.addEventListener("pointerup", handleClickOutside);
-    } else {
-      document.removeEventListener("pointerup", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("pointerup", handleClickOutside);
-    };
-  }, [handleClickOutside, isBadgeTooltipVisible]);
 
   type ContractExplorerInfoField = {
     id: string;
@@ -269,89 +245,6 @@ export const ContractInfo = ({
     }
   };
 
-  const renderBuildVerifiedBadge = (hasWasmData: boolean) => {
-    const item = {
-      verified: {
-        badge: (
-          <Badge variant="success" icon={<Icon.CheckCircle />}>
-            Build Verified
-          </Badge>
-        ),
-        message: (
-          <>
-            <code>Build Verified</code> means that a GitHub Action run has
-            attested to have built the Wasm, but does not verify the source
-            code.
-          </>
-        ),
-      },
-      unverified: {
-        badge: (
-          <Badge variant="error" icon={<Icon.XCircle />}>
-            Build Unverified
-          </Badge>
-        ),
-        message: (
-          <>
-            This contract has no build verification configured. Please see{" "}
-            <Link href="https://stellar.expert/explorer/public/contract/validation">
-              verification setup instructions
-            </Link>{" "}
-            for more info.
-          </>
-        ),
-      },
-      builtIn: {
-        badge: (
-          <Badge variant="success" icon={<Icon.CheckCircle />}>
-            Built-in Contract
-          </Badge>
-        ),
-        message: (
-          <>
-            The Stellar Asset Contract (SAC) is a built-in smart contract that
-            provides a standardized, programmatic interface to Stellar assets on
-            the network. It implements the{" "}
-            <Link href="https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0041.md">
-              SEP‑41 Token Interface
-            </Link>{" "}
-            via{" "}
-            <Link href="https://github.com/stellar/stellar-protocol/blob/master/core/cap-0046-06.md">
-              CAP-46-06
-            </Link>
-            .
-          </>
-        ),
-      },
-    };
-
-    const badge = isSacType
-      ? item.builtIn
-      : hasWasmData
-        ? item.verified
-        : item.unverified;
-
-    return (
-      <Tooltip
-        triggerEl={
-          <button
-            ref={buttonRef}
-            className="ContractInfo__badgeButton"
-            onClick={() => {
-              setIsBadgeTooltipVisible(!isBadgeTooltipVisible);
-            }}
-            type="button"
-          >
-            {badge.badge}
-          </button>
-        }
-        isVisible={isBadgeTooltipVisible}
-      >
-        {badge.message}
-      </Tooltip>
-    );
-  };
-
   if (isLoading) {
     return (
       <Box gap="sm" direction="row" justify="center">
@@ -394,7 +287,13 @@ export const ContractInfo = ({
               Contract
             </Text>
 
-            {infoData ? renderBuildVerifiedBadge(Boolean(wasmData)) : null}
+            {infoData ? (
+              <BuildVerifiedBadge
+                status={
+                  isSacType ? "builtIn" : wasmData ? "verified" : "unverified"
+                }
+              />
+            ) : null}
           </Box>
 
           <TabView
