324e52370c467ecfe2984b1a8175a00c55a810ff
Iveta <quietbits@users.noreply.github.com>
2025-12-03 13:17:49 -0500

    Transaction Explorer: Token Summary (#1731)
    * UI done with mock data
    
    * Fix tests
    
    * Update address style
    
    * Use real data
    
    * Tests added
    
    * Handle no asset case
    
    * Fix StellarExpert link

---
+++ src/app/(sidebar)/transaction-dashboard/components/TokenSummary.tsx
@@ -0,0 +1,216 @@
+import { Fragment } from "react";
+import { CopyText, Icon, IconButton, Link, Text } from "@stellar/design-system";
+import { BigNumber } from "bignumber.js";
+
+import { Box } from "@/components/layout/Box";
+import { DataTable } from "@/components/DataTable";
+import { TransactionTabEmptyMessage } from "@/components/TransactionTabEmptyMessage";
+
+import { formatAmount } from "@/helpers/formatAmount";
+import { shortenStellarAddress } from "@/helpers/shortenStellarAddress";
+import { getStellarExpertNetwork } from "@/helpers/getStellarExpertNetwork";
+
+import { STELLAR_EXPERT } from "@/constants/settings";
+import { useStore } from "@/store/useStore";
+
+import { RpcTxJsonResponse } from "@/types/types";
+
+export const TokenSummary = ({
+  txDetails,
+}: {
+  txDetails: RpcTxJsonResponse | null | undefined;
+}) => {
+  type TokenItem = {
+    addressFrom: string;
+    addressTo: string;
+    amount: string;
+    assetCode: string;
+    assetIssuer: string;
+  };
+
+  type EventGroupsType = {
+    native: {
+      id: string;
+      title: string;
+      items: TokenItem[];
+    };
+    asset: {
+      id: string;
+      title: string;
+      items: TokenItem[];
+    };
+  };
+
+  const getGroupedTransferEvents = () => {
+    const formattedEvents = getTransferEvents(txDetails);
+
+    const groups: EventGroupsType = {
+      asset: {
+        id: "asset-ev",
+        title: "Contract token transferred",
+        items: [],
+      },
+      native: {
+        id: "native-ev",
+        title: "Native token (XLM) transferred",
+        items: [],
+      },
+    };
+
+    formattedEvents.forEach((event) => {
+      if (event.assetCode === "XLM" && event.assetIssuer === "native") {
+        groups.native.items.push(event);
+      } else {
+        groups.asset.items.push(event);
+      }
+    });
+
+    // Return only groups with items
+    return Object.values(groups).filter((group) => group.items.length);
+  };
+
+  const { network } = useStore();
+
+  const seNetwork = getStellarExpertNetwork(network.id);
+
+  const formatAssetAddress = (address: string) => {
+    const isContractAddress = address.startsWith("C");
+
+    return (
+      <Box
+        gap="sm"
+        direction="row"
+        align="center"
+        addlClassName="TransactionTokenSummary__address"
+      >
+        <Link
+          href={`${STELLAR_EXPERT}/${seNetwork}/${isContractAddress ? "contract" : "account"}/${address}`}
+        >
+          {shortenStellarAddress(address)}
+        </Link>
+
+        <CopyText textToCopy={address} tooltipPlacement="top">
+          <IconButton
+            icon={<Icon.Copy01 />}
+            altText="Copy address"
+            customSize="1rem"
+          />
+        </CopyText>
+      </Box>
+    );
+  };
+
+  const formatAssetAmount = (amount: string) => {
+    return (
+      <span className="TransactionTokenSummary__amount">
+        {formatAmount(
+          new BigNumber(amount).dividedBy(10_000_000).toNumber(),
+          2,
+        )}
+      </span>
+    );
+  };
+
+  const formatAsset = (code: string, issuer: string) => {
+    if (!code || !issuer) {
+      return null;
+    }
+
+    const assetString =
+      code === "XLM" && issuer === "native" ? code : `${code}-${issuer}`;
+
+    return (
+      <Link
+        href={`${STELLAR_EXPERT}/${seNetwork}/asset/${assetString}`}
+        icon={<Icon.LinkExternal01 />}
+        iconPosition="right"
+      >
+        {code}
+      </Link>
+    );
+  };
+
+  const groupedTransferEvents = getGroupedTransferEvents();
+
+  if (!groupedTransferEvents.length) {
+    return (
+      <TransactionTabEmptyMessage>
+        There are no transfer events in this transaction
+      </TransactionTabEmptyMessage>
+    );
+  }
+
+  return (
+    <Box gap="lg" addlClassName="TransactionTokenSummary">
+      {groupedTransferEvents.map((ev) => (
+        <Fragment key={ev.id}>
+          <Box gap="md" data-testid={ev.id}>
+            <Text as="div" size="sm" weight="medium">
+              {ev.title}
+            </Text>
+
+            <DataTable
+              tableId="tx-dash-token-summary-assets"
+              tableData={ev.items}
+              tableHeaders={[
+                { id: "address-from", value: "From" },
+                { id: "address-to", value: "To" },
+                { id: "amount", value: "Amount" },
+                { id: "asset", value: "Asset" },
+              ]}
+              formatDataRow={(ti: TokenItem) => [
+                { value: formatAssetAddress(ti.addressFrom), isOverflow: true },
+                { value: formatAssetAddress(ti.addressTo), isOverflow: true },
+                { value: formatAssetAmount(ti.amount) },
+                { value: formatAsset(ti.assetCode, ti.assetIssuer) },
+              ]}
+              cssGridTemplateColumns="minmax(210px, 1fr) minmax(210px, 1fr) minmax(160px, 1fr) minmax(110px, 1fr)"
+              hidePagination={true}
+            />
+          </Box>
+        </Fragment>
+      ))}
+    </Box>
+  );
+};
+
+const getTransferEvents = (data: RpcTxJsonResponse | null | undefined) => {
+  const events = data?.events?.contractEventsJson;
+
+  if (!events) {
+    return [];
+  }
+
+  const contractEvents = Array.isArray(events[0])
+    ? events.flatMap((group: any[]) => group)
+    : events;
+
+  return contractEvents
+    .filter((event: any) => event?.body?.v0?.topics?.[0]?.symbol === "transfer")
+    .map((ev: any) => {
+      const addressFrom = ev.body.v0.topics[1].address;
+      const addressTo = ev.body.v0.topics[2].address;
+
+      const token = ev.body.v0.topics?.[3]?.string;
+
+      let assetCode = "";
+      let assetIssuer = "";
+
+      if (token) {
+        [assetCode, assetIssuer] = token.split(":");
+      }
+
+      return {
+        addressFrom,
+        addressTo,
+        // Transfer event data type https://github.com/stellar/stellar-protocol/blob/master/ecosystem/sep-0041.md#transfer-event
+        amount:
+          ev.body.v0.data?.map?.[0]?.val?.i128 ||
+          ev.body.v0.data?.i128 ||
+          ev.body.v0.data?.amount ||
+          "0",
+        assetCode: assetCode === "native" ? "XLM" : assetCode,
+        assetIssuer: assetCode === "native" ? "native" : assetIssuer,
+      };
+    });
+};
