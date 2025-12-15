13ca9db550da40225889656686ff36a1100a97ce
Iveta <iveta@stellar.org>
2025-12-15 15:23:47 -0500

    Update positive int validation

--- src/validate/methods/getPositiveIntError.ts
+++ src/validate/methods/getPositiveIntError.ts
@@ -1,8 +1,10 @@
 export const getPositiveIntError = (value: string) => {
   if (value.toString().charAt(0) === "-") {
     return "Expected a positive number or zero.";
-  } else if (!value.toString().match(/^[0-9]*$/g)) {
+  } else if (value === "" || !value.toString().match(/^[0-9]*$/g)) {
     return "Expected a whole number.";
+  } else if (BigInt(value) > Number.MAX_SAFE_INTEGER) {
+    return "Number exceeds maximum safe integer.";
   }
 
   return false;
