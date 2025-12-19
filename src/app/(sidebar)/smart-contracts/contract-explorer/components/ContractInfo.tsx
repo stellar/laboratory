d9eac5e2390f4934ca45a66f09551ed6dd5305d9
Iveta <iveta@stellar.org>
2025-12-19 09:28:48 -0500

    Trigger PR preview

--- src/app/(sidebar)/smart-contracts/contract-explorer/components/ContractInfo.tsx
+++ src/app/(sidebar)/smart-contracts/contract-explorer/components/ContractInfo.tsx
@@ -358,7 +358,7 @@ export const ContractInfo = ({
                 />
               ) : (
                 <NoDataMessage>
-                  Contract storage is not available for selected network.
+                  Contract storage data is not available for selected network.
                 </NoDataMessage>
               ),
               isDisabled: !isDataLoaded,
