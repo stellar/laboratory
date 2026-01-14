a70fa3cbbd62ff026930638c65f7bb1d02e8256d
jeesun 지선 <jeesunikim@users.noreply.github.com>
2026-01-14 09:20:56 -0800

    Update src/helpers/truncateString.ts
    Co-authored-by: Iveta <quietbits@users.noreply.github.com>

--- src/helpers/truncateString.ts
+++ src/helpers/truncateString.ts
@@ -2,5 +2,5 @@ export const truncateString = (text: string, maxLength: number) => {
   if (text.length <= maxLength) {
     return text;
   }
-  return text.slice(0, maxLength) + "...";
+  return text.slice(0, maxLength) + "…";
 };
