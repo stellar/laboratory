5b8f9c541290e46fc65d537ad4379915e7c74889
Jeesun Kim <jeesunikim@users.noreply.github.com>
2026-06-26 15:10:36 -0700

    add a fallback for scheduled_maintenances

--- src/query/useMaintenanceData.ts
+++ src/query/useMaintenanceData.ts
@@ -11,7 +11,7 @@ export const useMaintenanceData = () => {
         );
         const scheduleResponseJson = await scheduleResponse.json();
 
-        return scheduleResponseJson.scheduled_maintenances;
+        return scheduleResponseJson.scheduled_maintenances || [];
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
       } catch (e) {
         throw Error("Failed to fetch testnet reset date.");
